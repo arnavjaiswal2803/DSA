8cfbd5533b6efaeed9a7109092acd42313821e59
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-09-12 16:04:00 +0530

    Time: 164 ms (74.66%), Space: 137.2 MB (92.69%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,17 +3,13 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> maximums;
-        multiset<int, greater<int>> window;
-
-        for (int i = 0; i < k; i++) window.insert(nums[i]);
-        maximums.push_back(*window.begin());
-
-        for (int i = k; i < n; i++) {
-            window.erase(window.find(nums[i - k]));
-            window.insert(nums[i]);
-            maximums.push_back(*window.begin());
+        deque<int> dq;
+        for (int i = 0; i < n; i++) {
+            if (!dq.empty() && dq.front() <= i - k) dq.pop_front();
+            while (!dq.empty() && nums[i] >= nums[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+            if (i >= k - 1) maximums.push_back(nums[dq.front()]);
         }
-        
         return maximums;
     }
 };
