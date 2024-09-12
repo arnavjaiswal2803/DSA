ae748a70e730f72059cc2f1e8d1f48bcea72cf7a
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-09-12 12:52:26 +0530

    Time: 19 ms (80.7%), Space: 26.9 MB (62.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        const int OUT_OF_RANGE_NUM = -1e9 - 1;
         int n = nums.size();
-        vector<int> nge(n, OUT_OF_RANGE_NUM);
+        vector<int> nge(n, -1);
         stack<int> st;
         for (int i = 2 * n - 1; i >= 0; i--) {
-            if (i < n && nge[i] != OUT_OF_RANGE_NUM) continue;
             while (!st.empty() && nums[i % n] >= st.top()) st.pop();
-            if (!st.empty()) nge[i % n] = st.top();
+            if (i < n && !st.empty()) nge[i] = st.top();
             st.push(nums[i % n]);
-        }
-        for (int i = 0; i < n; i++) {
-            if (nge[i] == OUT_OF_RANGE_NUM) nge[i] = -1;
         }
         return nge;
     }
