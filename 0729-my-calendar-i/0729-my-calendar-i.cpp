1c06b6363a965e8ab70fa5cf210da5263446ccc9
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-09-26 11:51:54 +0530

    Time: 85 ms (48.46%), Space: 41.6 MB (86.63%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,40 @@
+class Booking {
+    int start;
+    int end;
+public:
+    Booking(int startt, int endd) {
+        start = startt;
+        end = endd;
+    }
+
+    int getStart() { return start; }
+
+    int getEnd() { return end; }
+};
+
+class MyCalendar {
+    vector<Booking> bookings;
+
+    bool doesIntersect(Booking booking, int start, int end) {
+        return (start >= booking.getStart() && start < booking.getEnd()) || 
+            (end - 1 >= booking.getStart() && end <= booking.getEnd()) ||
+            (start <= booking.getStart() && end >= booking.getEnd());
+    }
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        for (Booking &booking : bookings) 
+            if (doesIntersect(booking, start, end)) return false;
+        bookings.push_back(Booking{start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
