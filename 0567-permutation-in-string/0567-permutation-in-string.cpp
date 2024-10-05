c7a6644bea11410dcdd9fd42a117facc7bce86ba
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-06 01:47:06 +0530

    Time: 4 ms (81.5%), Space: 8.6 MB (98.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,23 +3,32 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.size(), m = s2.size();
         if (m < n) return false;
-        
-        unordered_map<char, int> freq;
-        for (auto ch : s1) freq[ch]++;
 
-        for (int i = 0; i <= m - n; i++) {
-            unordered_map<char, int> dupFreq = freq;
-            int matchCount = 0;
-            for (int j = i; j < m; j++) {
-                char ch = s2[j];
-                if (dupFreq.find(ch) != dupFreq.end() && dupFreq[ch] > 0) {
-                    matchCount++;
-                    dupFreq[ch]--;
-                } else break;
-            }
-            if (matchCount == n) return true;
+        vector<int> s1Freq(26, 0), s2Freq(26, 0);
+        for (int i = 0; i < n; i++) {
+            s1Freq[s1[i] - 'a']++;
+            s2Freq[s2[i] - 'a']++;
         }
 
-        return false;
+        int matchCount = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1Freq[i] == s2Freq[i]) matchCount++;
+        }
+
+        for (int i = 0; i < m - n; i++) {
+            if (matchCount == 26) return true;
+
+            int rightCh = s2[i + n], leftCh = s2[i];
+
+            s2Freq[rightCh - 'a']++;
+            if (s2Freq[rightCh - 'a'] == s1Freq[rightCh - 'a']) matchCount++;
+            else if (s2Freq[rightCh - 'a'] - 1 == s1Freq[rightCh - 'a']) matchCount--;
+
+            s2Freq[leftCh - 'a']--;
+            if (s2Freq[leftCh - 'a'] == s1Freq[leftCh - 'a']) matchCount++;
+            else if (s2Freq[leftCh - 'a'] + 1 == s1Freq[leftCh - 'a']) matchCount--;
+        }
+
+        return matchCount == 26;
     }
 }; 
