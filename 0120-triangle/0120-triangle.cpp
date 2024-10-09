ab659c618aef4ffe2d4edbdbafc967bbade17eff
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-09 14:42:32 +0530

    Time: 5 ms (39.04%), Space: 11.8 MB (35.55%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,17 +1,17 @@
 class Solution {
-    int minSum(int row, int col, int maxRow, vector<vector<int>>& triangle, 
-        vector<vector<int>>& dp) {
-            if (row == maxRow) return triangle[row][col];
-            if (dp[row][col] != -1) return dp[row][col];
-
-            return dp[row][col] =  triangle[row][col] + 
-                min(minSum(row + 1, col, maxRow, triangle, dp), 
-                    minSum(row + 1, col + 1, maxRow, triangle, dp));
-        }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        return minSum(0, 0, n - 1, triangle, dp);
+
+        for (int row = n - 1; row >= 0; row--) {
+            for (int col = 0; col <= row; col++) {
+                dp[row][col] = triangle[row][col];
+                if (row < n - 1) 
+                    dp[row][col] += min(dp[row + 1][col], dp[row + 1][col + 1]);
+            }
+        }
+
+        return dp[0][0];
     }
 };
