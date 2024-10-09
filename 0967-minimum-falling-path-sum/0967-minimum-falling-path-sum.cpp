f9db235352c8cf85d6fc8b804e3e29c2d5ad477d
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-09 16:52:05 +0530

    Time: 12 ms (59.55%), Space: 14 MB (26.56%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    int minSum(int row, int col, int maxCol, vector<vector<int>> &matrix, 
-        vector<vector<int>> &dp) {
-            if (col < 0 || col > maxCol) return INT_MAX;
-            if (row == 0) return matrix[row][col];
-            if (dp[row][col] != 1e9) return dp[row][col]; 
-
-            return dp[row][col] = matrix[row][col] + min({
-                minSum(row - 1, col - 1, maxCol, matrix, dp), 
-                minSum(row - 1, col, maxCol, matrix, dp), 
-                minSum(row - 1, col + 1, maxCol, matrix, dp)
-            });
-    }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+        int n = matrix.size(), ans = INT_MAX;
         vector<vector<int>> dp(n, vector<int>(n, 1e9));
-        int ans = INT_MAX;
-        for (int col = 0; col < n; col++) 
-            ans = min(ans, minSum(n - 1, col, n - 1, matrix, dp));
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (row == 0) dp[row][col] = matrix[row][col];
+                else {
+                    int prevSum = dp[row - 1][col];
+                    if (col > 0) prevSum = min(prevSum, dp[row - 1][col - 1]);
+                    if (col < n - 1) prevSum = min(prevSum, dp[row - 1][col + 1]);
+
+                    dp[row][col] = matrix[row][col] + prevSum;
+                }
+                if (row == n - 1) ans = min(ans, dp[n - 1][col]);
+            }
+        }
+
         return ans;
     }
 };
