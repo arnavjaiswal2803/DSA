cd4f6bf42172b3b4ed47bce73cfe7bff585c31ed
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-11 11:54:06 +0530

    Time: 62 ms (26.57%), Space: 47 MB (25.09%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
-        int n = nums.size();
+        int n = nums.size(), maxRamp = 0;
 
-        vector<int>rightMax(n);
-        rightMax[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) rightMax[i] = max(nums[i], rightMax[i + 1]);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            if (st.empty() || nums[i] < nums[st.top()]) st.push(i);
+        }
 
-        int left = 0, right = 0, maxRamp = 0;
-        while (right < n) {
-            while (left < right && nums[left] > rightMax[right]) left++;
-            maxRamp = max(maxRamp, right - left);
-            right++;
+        for (int i = n - 1; i >= 0 && !st.empty(); i--) {
+            while (!st.empty() && nums[i] >= nums[st.top()]) {
+                maxRamp = max(maxRamp, i - st.top());
+                st.pop();
+            }
         }
 
         return maxRamp;
