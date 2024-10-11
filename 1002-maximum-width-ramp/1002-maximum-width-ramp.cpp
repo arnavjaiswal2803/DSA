2a23f5550643c6ed070afad8457f46fc01c704f6
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-11 12:04:48 +0530

    Time: 61 ms (27.92%), Space: 47.1 MB (25.09%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -4,13 +4,13 @@ public:
         int n = nums.size(), maxRamp = 0;
 
         stack<int> st;
-        for (int i = 0; i < n; i++) {
-            if (st.empty() || nums[i] < nums[st.top()]) st.push(i);
+        for (int i = n - 1; i >= 0; i--) {
+            if (st.empty() || nums[i] > nums[st.top()]) st.push(i);
         }
 
-        for (int i = n - 1; i >= 0 && !st.empty(); i--) {
-            while (!st.empty() && nums[i] >= nums[st.top()]) {
-                maxRamp = max(maxRamp, i - st.top());
+        for (int i = 0; i < n && !st.empty(); i++) {
+            while (!st.empty() && nums[i] <= nums[st.top()]) {
+                maxRamp = max(maxRamp, st.top() - i);
                 st.pop();
             }
         }
