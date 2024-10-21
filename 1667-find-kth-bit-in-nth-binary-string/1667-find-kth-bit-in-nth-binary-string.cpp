c5f7f4d10252197cc2d139645534861806a303d6
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-21 17:33:21 +0530

    Time: 0 ms (100%), Space: 7.4 MB (81.37%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        if (n == 1) return '0';
+        bool isInvert = false;
+        int currLen = (1 << n) - 1;
 
-        int prevLen = pow(2, n - 1) - 1;
-        if (k == prevLen + 1) return '1';
-        else if (k <= prevLen) return findKthBit(n - 1, k);
-        else return findKthBit(n - 1, 2 * prevLen - k + 2) == '0' ? '1' : '0';
+        while (k > 1) {
+            if (k == (currLen / 2) + 1) return isInvert ? '0' : '1';
+            if (k > currLen / 2) {
+                k = currLen - k + 1;
+                isInvert = !isInvert; 
+            }
+            currLen /= 2;
+        }
+
+        return isInvert ? '1' : '0';
     }
 };
