5c23c7ace8495428f9d010ae8dc5bf760c9603f9
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-23 14:40:54 +0530

    Time: 7 ms (99.55%), Space: 317.3 MB (26.5%) - LeetHub

--- 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -2,7 +2,7 @@
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
- *     TreeNode *left;
+ *     TreeNode *right;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
@@ -10,36 +10,34 @@
  * };
  */
 class Solution {
-    struct nodeParent {
-        TreeNode* node;
-        TreeNode* parent;
-    };
 public:
     TreeNode* replaceValueInTree(TreeNode* root) {
-        queue<nodeParent> q;
-        unordered_map<TreeNode*, int> parentSum;
-        parentSum[NULL] = root->val;
+        queue<TreeNode*> q;
         int currLevelSum = root->val;
 
-        q.push({root, NULL});
+        q.push(root);
         while (!q.empty()) {
             int nextLevelSum = 0, k = q.size();
 
             for (int i = 0; i < k; i++) {
-                nodeParent temp = q.front();
+                TreeNode* node = q.front();
                 q.pop();
 
-                temp.node->val = currLevelSum - parentSum[temp.parent];
+                node->val = currLevelSum - node->val;
 
-                if (temp.node->left) {
-                    nextLevelSum += temp.node->left->val;
-                    parentSum[temp.node] += temp.node->left->val;
-                    q.push({temp.node->left, temp.node});
+                int siblingSum = 0;
+                if (node->left) siblingSum += node->left->val;
+                if (node->right) siblingSum += node->right->val;
+
+                if (node->left) {
+                    nextLevelSum += node->left->val;
+                    node->left->val = siblingSum;
+                    q.push(node->left);
                 }
-                if (temp.node->right) {
-                    nextLevelSum += temp.node->right->val;
-                    parentSum[temp.node] += temp.node->right->val;
-                    q.push({temp.node->right, temp.node});
+                if (node->right) {
+                    nextLevelSum += node->right->val;
+                    node->right->val = siblingSum;
+                    q.push(node->right);
                 }
             }
 
