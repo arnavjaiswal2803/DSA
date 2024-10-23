d3a180411ea125db32499752ffb0bee535624316
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-23 17:58:57 +0530

    Time: 0 ms (100%), Space: 7.8 MB (27.28%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.cpp
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -1,48 +1,37 @@
 class Solution {
+    typedef pair<int, char> pic;
 public:
     string longestDiverseString(int a, int b, int c) {
-        priority_queue<pair<int, char>> pq;
-        // Add the counts of a, b and c in priority queue.
-        if (a > 0) {
-            pq.push({a, 'a'});
-        }
+        priority_queue<pic, vector<pic>> pq;
+        if (a > 0) pq.push({a, 'a'});
+        if (b > 0) pq.push({b, 'b'});
+        if (c > 0) pq.push({c, 'c'});
 
-        if (b > 0) {
-            pq.push({b, 'b'});
-        }
-
-        if (c > 0) {
-            pq.push({c, 'c'});
-        }
-
-        string ans = "";
+        string str = "";
         while (!pq.empty()) {
-            pair<int, char> p = pq.top();
+            pic top = pq.top();
             pq.pop();
-            int count = p.first;
-            char character = p.second;
-            // If three consecutive characters exist, pick the second most
-            // frequent character.
-            if (ans.length() >= 2 and ans[ans.length() - 1] == p.second and
-                ans[ans.length() - 2] == p.second) {
+
+            int n = str.length();
+            char mostFreqChar = top.second;
+
+            if (n >= 2 && str[n - 1] == mostFreqChar && str[n - 2] == mostFreqChar) {
                 if (pq.empty()) break;
 
-                pair<int, char> temp = pq.top();
+                pic currTop = pq.top();
                 pq.pop();
-                ans += temp.second;
-                if (temp.first - 1 > 0) {
-                    pq.push({temp.first - 1, temp.second});
-                }
-            } else {
-                count--;
-                ans = ans + character;
-            }
 
-            // If count is greater than zero, add it to priority queue.
-            if (count > 0) {
-                pq.push({count, character});
+                str += currTop.second;
+                int newFreq = currTop.first - 1;
+                if (newFreq > 0) pq.push({newFreq, currTop.second});
+
             }
+            
+            str += mostFreqChar;
+            int updatedFreq = top.first - 1;
+            if (updatedFreq > 0) pq.push({updatedFreq, mostFreqChar});
         }
-        return ans;
+
+        return str;
     }
 };
