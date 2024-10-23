67faf10ad4303b27548ee78197bbffdac609b684
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-23 16:36:40 +0530

    Time: 0 ms (100%), Space: 7.6 MB (35.26%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -2,19 +2,24 @@ class Solution {
 public:
     int maximumSwap(int num) {
         string str = to_string(num);
-        int n = str.length();
+        int n = str.length(), maxIdx = n - 1;
+
+        vector<int> greater(n);
+        greater[n - 1] = maxIdx;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (str[i] > str[maxIdx]) maxIdx = i;
+            greater[i] = maxIdx;
+        }
 
         for (int i = 0; i < n - 1; i++) {
-            int maxIdx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (str[j] >= str[maxIdx]) maxIdx = j;
-            }
-
-            if (maxIdx != i && str[maxIdx] != str[i]) {
-                swap(str[i], str[maxIdx]);
+            if (str[greater[i]] > str[i]) {
+                swap(str[greater[i]], str[i]);
                 break;
             }
         }
+
+
 
         return stoi(str);
     }
