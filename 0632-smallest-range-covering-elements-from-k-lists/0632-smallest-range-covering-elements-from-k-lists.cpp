089b0601dfda7f277776220aa1c88eafa502e1ae
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-25 00:07:11 +0530

    Time: 47 ms (55.54%), Space: 29.3 MB (6.4%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,41 +1,34 @@
 class Solution {
+    typedef pair<int, pair<int, int>> pi;
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        int k = nums.size();
-        // Stores the current index of each list
-        vector<int> indices(k, 0);
-        // To track the smallest range
-        vector<int> range = {0, INT_MAX};
+        int k = nums.size(), rangeStart = 0, rangeEnd = INT_MAX, maxVal = INT_MIN;
+        priority_queue<pi, vector<pi>, greater<pi>> pq;
 
-        while (true) {
-            int curMin = INT_MAX, curMax = INT_MIN, minListIndex = 0;
-
-            // Find the current minimum and maximum values across the lists
-            for (int i = 0; i < k; i++) {
-                int currentElement = nums[i][indices[i]];
-
-                // Update the current minimum
-                if (currentElement < curMin) {
-                    curMin = currentElement;
-                    minListIndex = i;
-                }
-
-                // Update the current maximum
-                if (currentElement > curMax) {
-                    curMax = currentElement;
-                }
-            }
-
-            // Update the range if a smaller one is found
-            if (curMax - curMin < range[1] - range[0]) {
-                range[0] = curMin;
-                range[1] = curMax;
-            }
-
-            // Move to the next element in the list that had the minimum value
-            if (++indices[minListIndex] == nums[minListIndex].size()) break;
+        for (int i = 0; i < k; i++) {
+            pq.push({nums[i][0], {i, 0}});
+            maxVal = max(maxVal, nums[i][0]);
         }
 
-        return range;
+        while (pq.size() == k) {
+            pi elem = pq.top();
+            pq.pop();
+
+            int smallestVal = elem.first;
+            int row = elem.second.first, col = elem.second.second;
+
+            if (maxVal - smallestVal < rangeEnd - rangeStart) {
+                rangeStart = smallestVal;
+                rangeEnd = maxVal;
+            }
+
+            if (col + 1 < nums[row].size()) {
+                int newVal = nums[row][col + 1];
+                pq.push({newVal, {row, col + 1}});
+                maxVal = max(maxVal, newVal);
+            }
+        }
+
+        return {rangeStart, rangeEnd};
     }
 };
