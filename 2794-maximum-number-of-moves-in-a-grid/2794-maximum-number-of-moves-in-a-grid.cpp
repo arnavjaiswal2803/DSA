41efd971f973fec67840e492262abea75c41794a
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-10-30 10:24:44 +0530

    Time: 453 ms (6.1%), Space: 196.5 MB (5.52%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int maxMoves(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size(), ans = 0;
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<int> prev(n, 0);
 
         for (int col = m - 2; col >= 0; col--) {
+            vector<int> curr(n, 0);
             for (int row = 0; row < n; row++) {
                 int maxMoves = 0;
                 for (int i = 0; i < 3; i++) {
@@ -13,12 +14,13 @@ public:
                     
                     if (newRow >= 0 && newRow < n && newCol < m && 
                         grid[row][col] < grid[newRow][newCol])
-                            maxMoves = max(maxMoves, 1 + dp[newRow][newCol]);
+                            maxMoves = max(maxMoves, 1 + prev[newRow]);
                 }
-                dp[row][col] = maxMoves;
+                curr[row] = maxMoves;
 
-                if (col == 0) ans = max(ans, dp[row][col]);
+                if (col == 0) ans = max(ans, curr[row]);
             }
+            prev = curr;
         }
 
         return ans;
