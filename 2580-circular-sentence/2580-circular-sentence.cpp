9fd2ef26ba1410110f17764e6045fcdf2b1cefd7
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-02 23:44:39 +0530

    Time: 0 ms (100%), Space: 8.3 MB (59.78%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     bool isCircularSentence(string sentence) {
         int n = sentence.length();
-        char firstLetter, lastLetter;
         for (int i = 0; i < n; i++) {
-            if (i == 0) firstLetter = sentence[i];
-            if (i == n - 1) lastLetter = sentence[i];
             if (sentence[i] == ' ' && sentence[i-1] != sentence[i+1]) return false;
         }
-        return firstLetter == lastLetter;
+        return sentence[0] == sentence[n - 1];;
     }
 };
