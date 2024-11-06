807bc1c02998034179b3e1f1b7afc82087451655
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-06 14:18:40 +0530

    Time: 3 ms (43.24%), Space: 12.1 MB (67.88%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int minChanges(string s) {
-        int zeroCount = 0, oneCount = 0, changes = 0, n = s.length();
-        for (int i = 0; i < n; i++) {
-            s[i] == '0' ? zeroCount++ : oneCount++;
-
-            if (i & 1) {
-                if (zeroCount & 1) changes++;
-                zeroCount = 0;
-                oneCount = 0;
-            }
-        }
-        return changes;
+        int minChanges = 0, n = s.length();
+        for (int i = 0; i < n; i += 2) if (s[i] != s[i + 1]) minChanges++;
+        return minChanges;
     }
 };
