56a608e9ccc041a3539aa15ff0142f44f50e3dfa
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-06 18:11:20 +0530

    Time: 0 ms (100%), Space: 31.2 MB (63.03%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,25 +1,25 @@
 class Solution {
-    bool getBit(int num, int pos) { return ((num >> pos) & 1) > 0; }
-
-    int countSetBits(int n) {
-        int count = 0;
-        for (int i = 0; i <= 31; i++) if (getBit(n, i)) count++;
-        return count;
-    }
 public:
     bool canSortArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dup(nums);
+        int n = nums.size(), numOfSetBits = __builtin_popcount(nums[0]), 
+            maxOfSegment = nums[0], minOfSegment = nums[0], 
+            maxOfPrevSegment = INT_MIN;
 
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                if (dup[j] > dup[j + 1]) {
-                    if (countSetBits(dup[j]) != countSetBits(dup[j + 1])) 
-                        return false;
-                    swap(dup[j], dup[j + 1]);
-                }
+            if (__builtin_popcount(nums[i]) == numOfSetBits) {
+                maxOfSegment = max(maxOfSegment, nums[i]);
+                minOfSegment = min(minOfSegment, nums[i]);
+            } else {
+                if (minOfSegment < maxOfPrevSegment) return false;
+
+                maxOfPrevSegment = maxOfSegment;
+
+                numOfSetBits = __builtin_popcount(nums[i]);
+                maxOfSegment = nums[i];
+                minOfSegment = nums[i];
             }
         }
+        if (minOfSegment < maxOfPrevSegment) return false;
 
         return true;
     }
