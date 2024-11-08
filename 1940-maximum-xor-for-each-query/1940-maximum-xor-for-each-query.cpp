3afcdf14794587053d3324d9b1a1c328f3f1ec51
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-08 15:28:24 +0530

    Time: 3 ms (67.62%), Space: 95.5 MB (53.7%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
@@ -1,7 +1,6 @@
 class Solution {
     bool getBit(int num, int pos) { return (num & (1 << pos)) > 0; }
     int clearBit(int num, int pos) { return (num & (~(1 << pos))); }
-    int setBit(int num, int pos) { return (num | (1 << pos)); }
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
         int xorr = 0, maxK = pow(2, maximumBit) - 1, n = nums.size();
@@ -12,7 +11,7 @@ public:
             int k = maxK;
 
             for (int pos = maximumBit - 1; pos >= 0; pos--) 
-                k = getBit(xorr, pos) ? clearBit(k, pos) : k = setBit(k, pos);
+                k = getBit(xorr, pos) ? clearBit(k, pos) : k;
 
             ans[n - 1 - i] = k;
         }
