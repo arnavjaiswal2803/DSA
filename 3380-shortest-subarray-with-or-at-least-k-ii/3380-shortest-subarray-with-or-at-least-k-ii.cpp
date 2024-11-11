f03bb67514b06f89c3668e9a06fec810cd5d4fcf
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-12 00:51:50 +0530

    Time: 67 ms (52.58%), Space: 88.2 MB (73.67%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,70 +1,38 @@
 class Solution {
+    int getBit(int num, int pos) { return (((num >> pos) & 1) > 0); }
+    int clearBit(int num, int pos) { return (num & (~(1 << pos))); }
+    int setBit(int num, int pos) { return (num | (1 << pos)); }
+
+    void incrementFreqAndUpdateMask(int num, int &mask, vector<int> &freq) {
+        for (int i = 0; i < 32; i++) {
+            if (getBit(num, i)) freq[i]++;
+            if (freq[i] == 1) mask = setBit(mask, i);
+        }
+    }
+
+    void decrementFreqAndUpdateMask(int num, int &mask, vector<int> &freq) {
+        for (int i = 0; i < 32; i++) {
+            if (getBit(num, i)) freq[i]--;
+            if (freq[i] == 0) mask = clearBit(mask, i);
+        }
+    }
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        int left = 1;
-        int right = nums.size();
-        int minLength = -1;
+        vector<int> freq(32, 0);
+        int n = nums.size(), mask = 0, start = 0, end = 0, minLen = n + 1;
 
-        // Binary search on the length of subarray
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        while (end < n) {
+            if (nums[end] >= k) return 1;
 
-            if (hasValidSubarray(nums, k, mid)) {
-                minLength = mid;
-                right = mid - 1;  // Try to find smaller length
-            } else {
-                left = mid + 1;  // Try larger length
+            incrementFreqAndUpdateMask(nums[end], mask, freq);
+            while (mask >= k && start <= end) {
+                minLen = min(minLen, end - start + 1);
+                decrementFreqAndUpdateMask(nums[start], mask, freq);
+                start++;
             }
+            end++;
         }
 
-        return minLength;
-    }
-
-private:
-    // Checks if there exists a subarray of given length whose bitwise OR is >=
-    // k
-    bool hasValidSubarray(vector<int>& nums, int targetSum, int windowSize) {
-        int arrayLength = nums.size();
-        vector<int> bitCounts(32,
-                              0);  // Tracks count of set bits at each position
-
-        // Sliding window approach
-        for (int right = 0; right < arrayLength; right++) {
-            // Add current number to window
-            updateBitCounts(bitCounts, nums[right], 1);
-
-            // Remove leftmost number if window exceeds size
-            if (right >= windowSize) {
-                updateBitCounts(bitCounts, nums[right - windowSize], -1);
-            }
-
-            // Check if current window is valid
-            if (right >= windowSize - 1 &&
-                convertBitCountsToNumber(bitCounts) >= targetSum) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // Updates bit count array when adding/removing a number from window
-    void updateBitCounts(vector<int>& bitCounts, int number, int delta) {
-        for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
-            // Check if bit is set at current position
-            if (((number >> bitPosition) & 1) != 0) {
-                bitCounts[bitPosition] += delta;
-            }
-        }
-    }
-
-    // Converts bit count array back to number using OR operation
-    int convertBitCountsToNumber(vector<int>& bitCounts) {
-        int number = 0;
-        for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
-            if (bitCounts[bitPosition] != 0) {
-                number |= 1 << bitPosition;
-            }
-        }
-        return number;
+        return minLen <= n ? minLen : -1;
     }
 };
