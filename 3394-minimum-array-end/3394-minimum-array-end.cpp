04868c7cf4c02dc90310785f5729b834235e9a22
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-11 23:49:03 +0530

    Time: 3 ms (39.14%), Space: 8.6 MB (35.97%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -2,7 +2,13 @@ class Solution {
 public:
     long long minEnd(int n, int x) {
         long long ans = x;
-        while (--n) ans = ((ans + 1) | x);
+        n--;
+        for (long long mask = 1; n > 0; mask <<= 1) {
+            if ((x & mask) == 0) {
+                ans |= ((n & 1) * mask);
+                n >>= 1;
+            }
+        }
         return ans;
     }
 };
