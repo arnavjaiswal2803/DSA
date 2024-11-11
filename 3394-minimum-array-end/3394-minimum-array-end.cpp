b26bce09e510da9d7e73ea5d7d4a06c5cf531dd1
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-11 17:30:46 +0530

    Time: 1738 ms (18.53%), Space: 8.4 MB (89.64%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        long long result = x;
-
-        // Step 1: Iterate n-1 times (since we already initialized result with
-        // x)
-        while (--n) {
-            // Step 2: Increment result and perform bitwise OR with x
-            result = (result + 1) | x;
-        }
-
-        return result;
+        long long ans = x;
+        while (--n) ans = ((ans + 1) | x);
+        return ans;
     }
 };
