8ec0d3d88c10c1766bc4f408f7f8ed37b0300df6
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-12 17:06:49 +0530

    Time: 99 ms (81.32%), Space: 98.5 MB (49.24%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,15 +1,4 @@
 class Solution {
-    bool subsetSum(vector<int> &nums, int idx, int target, vector<vector<int>> &dp) {
-        if (target == 0) return true;
-        if (idx == 0) return nums[idx] == target;
-        if (dp[idx][target] != -1) return dp[idx][target];
-
-        bool exclude = subsetSum(nums, idx - 1, target, dp), include = false;
-        if (!exclude && nums[idx] <= target) 
-            include = subsetSum(nums, idx - 1, target - nums[idx], dp);
-
-        return dp[idx][target] = include || exclude;
-    }
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size(), totalSum = 0;
@@ -17,21 +6,23 @@ public:
         for (int i = 0; i < n; i++) totalSum += nums[i];
         if (totalSum & 1) return false;
 
-        vector<vector<int>> dp(n, vector<int>(totalSum / 2 + 1, -1));
+        vector<int> prev(totalSum / 2 + 1);
         
         for (int idx = 0; idx < n; idx++) {
+            vector<int> curr(totalSum / 2 + 1);
             for (int target = 0; target <= totalSum / 2; target++) {
-                if (target == 0) dp[idx][target] = true;
-                else if (idx == 0) dp[idx][target] = nums[idx] == target;
+                if (target == 0) curr[target] = true;
+                else if (idx == 0) curr[target] = nums[idx] == target;
                 else {
-                    bool exclude = dp[idx - 1][target], include = false;
+                    bool exclude = prev[target], include = false;
                     if (!exclude && nums[idx] <= target) 
-                        include = dp[idx - 1][target - nums[idx]];
-                    dp[idx][target] = include || exclude;
+                        include = prev[target - nums[idx]];
+                    curr[target] = include || exclude;
                 }
             }
+            prev = curr;
         }
         
-        return subsetSum(nums, n - 1, totalSum / 2, dp);
+        return prev[totalSum / 2];
     }
 };
