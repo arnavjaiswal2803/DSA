215af09719a3ae7bdad38afa9737d0ea7d1ab857
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-12 12:52:40 +0530

    Time: 89 ms (18.85%), Space: 107.9 MB (13.51%) - LeetHub

--- 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
@@ -1,9 +1,20 @@
 class Solution {
+    int binarySearch(vector<vector<int>> &maxBeauty, int target) {
+        if (target < maxBeauty[0][0]) return 0;
+
+        int low = 0, high = maxBeauty.size() - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+
+            if (maxBeauty[mid][0] == target) return maxBeauty[mid][1];
+            else if (maxBeauty[mid][0] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return maxBeauty[high][1];
+    }
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
         int n = items.size(), m = queries.size();
-        vector<int> ans(m, 0);
-        map<int, int> maxiBeauty;
 
         sort(items.begin(), items.end(), [&](vector<int> &a, vector<int> &b) {
             if (a[0] < b[0]) return true;
@@ -11,20 +22,17 @@ public:
             else return a[1] > b[1];
         });
 
-        for (int i = 0; i < m; i++) ans[i] = maxiBeauty[queries[i]] = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || items[i][0] != items[i - 1][0]) 
-                maxiBeauty[items[i][0]] = items[i][1];
+        vector<vector<int>> maxBeauty = { items[0] };
+        for (int i = 1; i < n; i++) {
+            if (items[i][0] != items[i - 1][0]) {
+                vector<int> temp = { items[i][0], 
+                    max(maxBeauty.back()[1], items[i][1]) };
+                maxBeauty.push_back(temp);
+            }
         }
 
-        int maxBeautyTillCurrPrice = 0;
-        for(auto &it : maxiBeauty) {
-            maxBeautyTillCurrPrice = max(maxBeautyTillCurrPrice, it.second);
-            maxiBeauty[it.first] = maxBeautyTillCurrPrice;
-        }
-
-        for (int i = 0; i < m; i++) ans[i] = maxiBeauty[queries[i]];
+        vector<int> ans(m);
+        for (int i = 0; i < m; i++) ans[i] = binarySearch(maxBeauty, queries[i]);
 
         return ans;
     }
