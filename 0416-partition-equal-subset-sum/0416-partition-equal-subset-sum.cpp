98cfcdd2ad6b12c02b4132743ccc01b01c8bd398
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-12 16:58:43 +0530

    Time: 75 ms (85.97%), Space: 99.1 MB (30.4%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,18 +1,23 @@
 class Solution {
-    bool canPartition(vector<int> &nums, int idx, int sum1, int sum2, 
-        vector<vector<int>> &dp) {
-            if (sum1 == sum2) return true;
-            if (idx == 0) return sum1 + nums[idx] == sum2 - nums[idx];
-            if (dp[idx][sum1] != -1) return dp[idx][sum1];
+    bool subsetSum(vector<int> &nums, int idx, int target, vector<vector<int>> &dp) {
+        if (target == 0) return true;
+        if (idx == 0) return nums[idx] == target;
+        if (dp[idx][target] != -1) return dp[idx][target];
 
-            return dp[idx][sum1] = canPartition(nums, idx - 1, sum1, sum2, dp) || 
-                canPartition(nums, idx - 1, sum1 + nums[idx], sum2 - nums[idx], dp);
-        }
+        bool exclude = subsetSum(nums, idx - 1, target, dp), include = false;
+        if (!exclude && nums[idx] <= target) 
+            include = subsetSum(nums, idx - 1, target - nums[idx], dp);
+
+        return dp[idx][target] = include || exclude;
+    }
 public:
     bool canPartition(vector<int>& nums) {
-        int n = nums.size(), sum = 0;
-        for (int i = 0; i < n; i++) sum += nums[i];
-        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-        return canPartition(nums, n - 1, 0, sum, dp);
+        int n = nums.size(), totalSum = 0;
+
+        for (int i = 0; i < n; i++) totalSum += nums[i];
+        if (totalSum & 1) return false;
+
+        vector<vector<int>> dp(n, vector<int>(totalSum / 2 + 1, -1));
+        return subsetSum(nums, n - 1, totalSum / 2, dp);
     }
 };
