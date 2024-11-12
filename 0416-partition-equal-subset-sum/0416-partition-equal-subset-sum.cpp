877f2915594e2e123c76b772d9bb8606dccbd37c
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-12 17:04:19 +0530

    Time: 131 ms (73.37%), Space: 98.9 MB (39.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,4 +1,15 @@
 class Solution {
+    bool subsetSum(vector<int> &nums, int idx, int target, vector<vector<int>> &dp) {
+        if (target == 0) return true;
+        if (idx == 0) return nums[idx] == target;
+        if (dp[idx][target] != -1) return dp[idx][target];
+
+        bool exclude = subsetSum(nums, idx - 1, target, dp), include = false;
+        if (!exclude && nums[idx] <= target) 
+            include = subsetSum(nums, idx - 1, target - nums[idx], dp);
+
+        return dp[idx][target] = include || exclude;
+    }
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size(), totalSum = 0;
@@ -10,7 +21,7 @@ public:
         
         for (int idx = 0; idx < n; idx++) {
             for (int target = 0; target <= totalSum / 2; target++) {
-                if (target == 0) dp[idx][target] = 0;
+                if (target == 0) dp[idx][target] = true;
                 else if (idx == 0) dp[idx][target] = nums[idx] == target;
                 else {
                     bool exclude = dp[idx - 1][target], include = false;
@@ -20,7 +31,7 @@ public:
                 }
             }
         }
-
-        return dp[n - 1][totalSum / 2];
+        
+        return subsetSum(nums, n - 1, totalSum / 2, dp);
     }
 };
