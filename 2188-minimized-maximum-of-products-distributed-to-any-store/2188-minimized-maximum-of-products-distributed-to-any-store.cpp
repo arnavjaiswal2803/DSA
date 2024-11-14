abbc69a440dceea2aada54f6c083baf94123e9f1
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-15 00:28:13 +0530

    Time: 117 ms (26.22%), Space: 87.3 MB (72.13%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool canDistribute(vector<int> &quantities, int storeCnt, long long val) {
+    bool canDistribute(vector<int> &quantities, int storeCnt, int val) {
         int prod = 0, prodQty = quantities[prod];
         for (int i = 0; i < storeCnt; i++) {
             if (val < prodQty) prodQty -= val;
@@ -13,18 +13,11 @@ class Solution {
     }
 public:
     int minimizedMaximum(int n, vector<int>& quantities) {
-        int productsCount = quantities.size();
-        long long sum = 0;
-
-        for (int i = 0; i < productsCount; i++) sum += quantities[i];
-
-        long long low = sum / n, high = sum;
+        int low = 0, high = *max_element(quantities.begin(), quantities.end());
         while (low <= high) {
-            long long mid = low + ((high - low) / 2);
-
+            int mid = low + ((high - low) >> 1);
             canDistribute(quantities, n, mid) ? high = mid - 1 : low = mid + 1;
         }
-
-        return (int)low;
+        return low;
     }
 };
