a8ff44724eb318990bba66f5d8c829c373448f21
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-17 00:02:43 +0530

    Time: 4 ms (33.33%), Space: 34.2 MB (13.76%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -6,12 +6,9 @@ public:
         vector<int> ans(n - k + 1);
 
         for (int i = 0; i < n; i++) {
-            if (!dq.empty() && nums[i] != nums[dq.back()] + 1) {
-                while (!dq.empty()) dq.pop_front();
-            }
+            if (!dq.empty() && nums[i] != nums[dq.back()] + 1) dq.clear();
             dq.push_back(i);
-            if (i >= k - 1) 
-                ans [i - k + 1] = dq.size() == k ? nums[dq.back()] : -1;
+            if (i >= k - 1) ans [i - k + 1] = dq.size() == k ? nums[dq.back()] : -1;
             if (dq.size() == k) dq.pop_front();
         }
 
