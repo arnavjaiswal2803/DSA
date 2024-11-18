9bafacde35ec877b65aadbabf6e11dfad5d4e8fd
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-18 12:24:17 +0530

    Time: 0 ms (100%), Space: 10.3 MB (84%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -1,17 +1,31 @@
 class Solution {
+    int getTrueIdx(int idx, int n) { return (idx + n) % n; }
 public:
     vector<int> decrypt(vector<int>& code, int k) {
-        int n = code.size();
-        vector<int> ans(n, 0);
-
+        int n = code.size(), replacement = 0;
+        vector<int> ans(n, replacement);
         if (k == 0) return ans;
+
+        int start, end;
+        if (k > 0) {
+            start = 1;
+            end = 1;
+        } else {
+            start = k + n; 
+            end = k + n;
+        }
+
+        for (int i = 0; i < abs(k); i++) {
+            replacement += code[getTrueIdx(end, n)];
+            end++;
+        }
+
         for (int i = 0; i < n; i++) {
-            int replacement = 0;
-            for (int j = 1; j <= abs(k); j++) {
-                int newIdx = k > 0 ? (i + j) % n : (i - j + n) % n; 
-                replacement += code[newIdx];
-            }
             ans[i] = replacement;
+
+            replacement += (code[getTrueIdx(end, n)] - code[getTrueIdx(start, n)]);
+            ++start;
+            ++end;
         }
 
         return ans;
