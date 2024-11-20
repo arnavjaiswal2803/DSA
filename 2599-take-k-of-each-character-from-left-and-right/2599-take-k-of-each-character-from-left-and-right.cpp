e1ae001fe538cd7d2f620a5e542c7d52608f74da
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-20 14:12:58 +0530

    Time: 19 ms (49.76%), Space: 11.9 MB (96.89%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
@@ -2,33 +2,26 @@ class Solution {
     bool areCharEnough(vector<int> &charFreq, int k) {
         return charFreq[0] >= k && charFreq[1] >= k && charFreq[2] >= k;
     }
-
-    bool isPossible(string &s, int n, int k, int minutes) {
-        vector<int> charFreq(3, 0);
-
-        for (int i = 0; i < minutes; i++) charFreq[s[i] - 'a']++;
-        if (areCharEnough(charFreq, k)) return true;
-
-        for (int i = minutes - 1, j = n - 1; i >= 0; i--, j--) {
-            charFreq[s[i] - 'a']--;
-            charFreq[s[j] - 'a']++;
-            if (areCharEnough(charFreq, k)) return true;
-        }
-
-        return false;
-    }
 public:
     int takeCharacters(string s, int k) {
-        if (k == 0) return 0;
-        
-        int n = s.length(), low = 3, high = n;
-        while (low <= high) {
-            int mid = low + ((high - low) >> 1);
+        int n = s.size();
+        vector<int> charFreq(3, 0);
 
-            if (isPossible(s, n, k, mid)) high = mid - 1;
-            else low = mid + 1;
+        for (int i = 0; i < n; i++) charFreq[s[i] - 'a']++;
+        if (!areCharEnough(charFreq, k)) return -1;
+
+        int left = 0, right = 0, maxWindowSize = 0;
+        while (right < n) {
+            charFreq[s[right] - 'a']--;
+            while (!areCharEnough(charFreq, k)) {
+                charFreq[s[left] - 'a']++;
+                left++;
+            }
+            int windowSize = right - left + 1;
+            maxWindowSize = max(maxWindowSize, windowSize);
+            right++;
         }
-
-        return low <= n ? low : -1;
+        
+        return n - maxWindowSize;
     }
 };
