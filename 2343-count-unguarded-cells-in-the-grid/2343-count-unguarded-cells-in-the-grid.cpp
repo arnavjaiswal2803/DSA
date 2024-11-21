2cd2b5f14a34af9be6b85ac7dbef3ae47df22c6b
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-21 18:24:56 +0530

    Time: 75 ms (32.99%), Space: 172.3 MB (46.41%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -1,9 +1,14 @@
 class Solution {
+    const int UNGUARDED = 0;
+    const int GUARDED = 1;
+    const int GUARD = 2;
+    const int WALL = 3;
+
     void markWalls(vector<vector<int>> &cells, vector<vector<int>> &walls) {
         int wallsCount = walls.size();
         for (int i = 0; i < wallsCount; i++) {
             int wallRow = walls[i][0], wallCol = walls[i][1];
-            cells[wallRow][wallCol] = -1;
+            cells[wallRow][wallCol] = WALL;
         }
     }
 
@@ -11,47 +16,52 @@ class Solution {
         int guardsCount = guards.size();
         for (int i = 0; i < guardsCount; i++) {
             int guardRow = guards[i][0], guardCol = guards[i][1];
-            cells[guardRow][guardCol] = -1;
+            cells[guardRow][guardCol] =  GUARD;
         }
     }
 
-    void markGuardedCells(int m, int n, vector<vector<int>> &cells, vector<vector<int>> &guards) {
-        int guardsCount = guards.size();
-        for (int i = 0; i < guardsCount; i++) {
-            int guardRow = guards[i][0], guardCol = guards[i][1];
+    bool updateCellVisibility( vector<vector<int>> &cells, int row, int col, bool isGuardLineActive) {
+        if (cells[row][col] == GUARD) return true;
+        if (cells[row][col] == WALL) return false;
+        if (isGuardLineActive) cells[row][col] = GUARDED;
+        return isGuardLineActive;
+    }
 
-            for (int col = guardCol + 1; col < n; col++) {
-                if (cells[guardRow][col] == -1) break;
-                else cells[guardRow][col] = 0;
-            }
-            for (int col = guardCol - 1; col >= 0; col--) {
-                if (cells[guardRow][col] == -1) break;
-                else cells[guardRow][col] = 0;
-            }
-            for (int row = guardRow + 1; row < m; row++) {
-                if (cells[row][guardCol] == -1) break;
-                else cells[row][guardCol] = 0;
-            }
-            for (int row = guardRow - 1; row >= 0; row--) {
-                if (cells[row][guardCol] == -1) break;
-                else cells[row][guardCol] = 0;
-            }
+    void markGuardedCells(int m, int n, vector<vector<int>> &cells) {
+        for (int row = 0; row < m; row++) {
+            bool isGuardLineActive = cells[row][0] == GUARD;
+            for (int col = 1; col < n; col++) 
+                isGuardLineActive = updateCellVisibility(cells, row, col, isGuardLineActive);
+
+            isGuardLineActive = cells[row][n - 1] == GUARD;
+            for (int col = n - 2; col >= 0; col--) 
+                isGuardLineActive = updateCellVisibility(cells, row, col, isGuardLineActive);
+        }
+
+        for (int col = 0; col < n; col++) {
+            bool isGuardLineActive = cells[0][col] == GUARD;
+            for (int row = 1; row < m; row++) 
+                isGuardLineActive = updateCellVisibility(cells, row, col, isGuardLineActive);
+
+            isGuardLineActive = cells[m - 1][col] == GUARD;
+            for (int row = m - 2; row >= 0; row--) 
+                isGuardLineActive = updateCellVisibility(cells, row, col, isGuardLineActive);
         }
     }
 
     int countUnguardedCells(int m, int n, vector<vector<int>> &cells) {
         int unguardedCells = 0;
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) if (cells[i][j] == 1) unguardedCells++;
+            for (int j = 0; j < n; j++) if (cells[i][j] == UNGUARDED) unguardedCells++;
         }
         return unguardedCells;
     }
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        vector<vector<int>> cells(m, vector<int>(n, 1));
+        vector<vector<int>> cells(m, vector<int>(n, UNGUARDED));
         markWalls(cells, walls);
         markGuards(cells, guards);
-        markGuardedCells(m, n, cells, guards);
+        markGuardedCells(m, n, cells);
         return countUnguardedCells(m, n, cells);
     }
 };
