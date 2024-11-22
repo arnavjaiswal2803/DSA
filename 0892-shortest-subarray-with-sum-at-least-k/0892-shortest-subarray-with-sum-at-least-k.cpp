c37fed6cbbb005ed8c24de44706bcf91c16d782d
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-22 17:25:25 +0530

    Time: 40 ms (55.57%), Space: 107.6 MB (66.7%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,19 +1,20 @@
 class Solution {
-typedef pair<long long, int> pii;
 public:
     int shortestSubarray(vector<int>& nums, int k) {
         int n = nums.size(), minLen = INT_MAX;
         long long sum = 0;
-        priority_queue<pii, vector<pii>, greater<>> prefixSumHeap;
 
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            if (sum >= k) minLen = min(minLen, i + 1);
-            while (!prefixSumHeap.empty() && sum - prefixSumHeap.top().first >= k) {
-                minLen = min(minLen, i - prefixSumHeap.top().second);
-                prefixSumHeap.pop();
+        vector<long long> prefixSum(n + 1, 0);
+        for (int i = 1; i <= n; i++) prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+
+        deque<int> dq;
+        for (int i = 0; i <= n; i++) {
+            while (!dq.empty() && prefixSum[i] - prefixSum[dq.front()] >= k) {
+                minLen = min(minLen, i - dq.front());
+                dq.pop_front();
             }
-            prefixSumHeap.push({sum, i});
+            while (!dq.empty() && prefixSum[i] <= prefixSum[dq.back()]) dq.pop_back();
+            dq.emplace_back(i);
         }
 
         return minLen == INT_MAX ? -1 : minLen;
