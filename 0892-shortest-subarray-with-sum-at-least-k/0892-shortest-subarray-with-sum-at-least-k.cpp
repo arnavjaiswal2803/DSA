5f32da2c2f4a0608c77e69219c5d2dbbd1c84800
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-11-22 17:35:54 +0530

    Time: 42 ms (52.03%), Space: 107.6 MB (80.82%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -4,11 +4,13 @@ public:
         int n = nums.size(), minLen = INT_MAX;
         long long sum = 0;
 
-        vector<long long> prefixSum(n + 1, 0);
-        for (int i = 1; i <= n; i++) prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        vector<long long> prefixSum(n, 0);
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) prefixSum[i] = prefixSum[i - 1] + nums[i];
 
         deque<int> dq;
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
+            if (prefixSum[i] >= k) minLen = min(minLen, i + 1);
             while (!dq.empty() && prefixSum[i] - prefixSum[dq.front()] >= k) {
                 minLen = min(minLen, i - dq.front());
                 dq.pop_front();
