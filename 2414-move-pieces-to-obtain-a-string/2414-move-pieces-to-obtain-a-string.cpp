952cf6ab94f14a0522c358cbe707cdbc1f62599b
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-05 23:44:20 +0530

    Time: 13 ms (46.55%), Space: 21.9 MB (33.33%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     bool canChange(string start, string target) {
-        int n = start.size();
-        queue<pair<char, int>> s, t;
-
-        for (int i = 0; i < n; i++) {
-            if (start[i] != '_') s.push({start[i], i});
-            if (target[i] != '_') t.push({target[i], i});
-        }
-
-        if (s.size() != t.size()) return false;
-
-        while (!s.empty() && !t.empty()) {
-            char startCh = s.front().first, targetCh = t.front().first;
-            int startPos = s.front().second, targetPos = t.front().second;
-            s.pop();
-            t.pop();
-
-            if (startCh != targetCh || 
-                startCh == 'L' && startPos < targetPos || 
-                startCh == 'R' && startPos > targetPos) 
+        int n = start.size(), i = 0, j = 0;
+        
+        while (i < n && start[i] == '_') i++;
+        while (j < n && target[j] == '_') j++;
+        
+        while (i < n && j < n) {
+            if (start[i] != target[j] || 
+                (start[i] == 'L' && i < j) ||
+                (start[i] == 'R' && i > j))
                 return false;
+            
+            i++;
+            j++;
+            
+            while (i < n && start[i] == '_') i++;
+            while (j < n && target[j] == '_') j++;
         }
 
-        return true;
+        return i == n && j == n;
     }
 };
