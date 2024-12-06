7dc01cb2f11b200b9ad83bef23f3d5b61710fa72
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-06 16:39:25 +0530

    Time: 44 ms (91.4%), Space: 24.6 MB (32.89%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -7,26 +7,30 @@ public:
         const int INF = 1e9;
         int m = heights.size(), n = heights[0].size();
         vector<vector<int>> efforts(m, vector<int>(n, INF));
-        queue<pair<int, int>> q;
+        priority_queue<pair<int, pair<int, int>>, 
+            vector<pair<int, pair<int, int>>>, greater<>> pq;
 
-        q.push({0, 0});
+        pq.push({0, {0, 0}});
         efforts[0][0] = 0;
 
         vector<int> dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
-        while (!q.empty()) {
-            int x = q.front().first, y = q.front().second;
-            q.pop();
+        while (!pq.empty()) {
+            int diff = pq.top().first;
+            int x = pq.top().second.first, y = pq.top().second.second;
+            pq.pop();
+
+            if (x == m - 1 && y == n - 1) return diff;
 
             for (int i = 0; i < 4; i++) {
                 int newX = x + dx[i], newY = y + dy[i];
 
                 if (isValid(m, n, newX, newY)) {
-                    int currEffort = max(efforts[x][y], 
+                    int currEffort = max(diff, 
                         abs(heights[newX][newY] - heights[x][y]));
 
                     if (currEffort < efforts[newX][newY]) {
                         efforts[newX][newY] = currEffort;
-                        q.push({newX, newY});
+                        pq.push({efforts[newX][newY], {newX, newY}});
                     }
                 }
             }
