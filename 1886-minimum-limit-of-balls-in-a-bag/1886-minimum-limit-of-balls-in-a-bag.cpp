441b3293e7cc961cb813a7704d2aa7f9e34268d8
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-09 15:11:57 +0530

    Time: 74 ms (5.28%), Space: 59.9 MB (5.66%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,50 +1,26 @@
 class Solution {
-public:
-    int minimumSize(vector<int>& nums, int maxOperations) {
-        // Binary search bounds
-        int left = 1;
-        int right = 0;
-
-        for (auto num : nums) {
-            right = max(right, num);
-        }
-
-        // Perform binary search to find the optimal maxBallsInBag
-        while (left < right) {
-            int middle = (left + right) / 2;
-
-            // Check if a valid distribution is possible with the current middle
-            // value
-            if (isPossible(middle, nums, maxOperations)) {
-                right = middle;  // If possible, try a smaller value (shift
-                                 // right to middle)
-            } else {
-                left = middle + 1;  // If not possible, try a larger value
-                                    // (shift left to middle + 1)
-            }
-        }
-
-        // Return the smallest possible value for maxBallsInBag
-        return left;
-    }
-
-private:
-    // Helper function to check if a distribution is possible for a given
-    // maxBallsInBag
-    bool isPossible(int maxBallsInBag, vector<int>& nums, int maxOperations) {
+    bool isPossible(vector<int>& nums, int n, int maxOperations, int maxBalls) {
         int totalOperations = 0;
-
-        // Iterate through each bag in the array
-        for (int num : nums) {
-            // Calculate the number of operations needed to split this bag
-            int operations = ceil(num / (double)maxBallsInBag) - 1;
-            totalOperations += operations;
-
-            // Total operations exceed maxOperations, return false
+        for (int i = 0; i < n; i++) {
+            totalOperations += ceil(nums[i] / (double)maxBalls) - 1;
             if (totalOperations > maxOperations) return false;
         }
-
-        // We can split the balls within the allowed operations, return true
         return true;
+    }
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int n = nums.size(), largest = nums[0];
+
+        for (int i = 1; i < n; i++) largest = max(largest, nums[i]);
+
+        int low = 1, high = largest;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+
+            if (isPossible(nums, n, maxOperations, mid)) high = mid - 1;
+            else low = mid + 1;
+        }
+
+        return low;
     }
 };
