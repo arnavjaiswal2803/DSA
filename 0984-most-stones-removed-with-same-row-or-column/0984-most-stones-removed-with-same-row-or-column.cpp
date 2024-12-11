2d2abf209f8a457b95cf47c8f6647407ab84e087
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-11 11:53:27 +0530

    Time: 8 ms (90.43%), Space: 21.1 MB (63.09%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,61 @@
+class DisjointSet {
+    vector<int> size, parent;
+public:
+    DisjointSet(int n) {
+        size.resize(n + 1, 1);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) parent[i] = i;
+    }
+
+    int findParent(int i) {
+        if (parent[i] == i) return i;
+        return parent[i] = findParent(parent[i]);
+    }
+
+    void createUnion(int u, int v) {
+        int ultimateParentU = findParent(u);
+        int ultimateParentV = findParent(v);
+
+        if (ultimateParentU == ultimateParentV) return;
+
+        if (ultimateParentU <= ultimateParentV) {
+            parent[ultimateParentU] = ultimateParentV;
+            size[ultimateParentV] += size[ultimateParentU];
+        } else {
+            parent[ultimateParentV] = ultimateParentU;
+            size[ultimateParentU] += size[ultimateParentV];
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        DisjointSet ds(n);
+        unordered_map<int, int> rowMap, colMap;
+
+        for (int i = 0; i < n; i++) {
+            int row = stones[i][0], col = stones[i][1];
+
+            if (rowMap.find(row) == rowMap.end()) {
+                rowMap[row] = i;
+            } else {
+                ds.createUnion(rowMap[row], i);
+            }
+
+            if (colMap.find(col) == colMap.end()) {
+                colMap[col] = i;
+            } else {
+                ds.createUnion(colMap[col], i);
+            }
+        }
+
+        int cannotRemove = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.findParent(i) == i) cannotRemove++;
+        }
+
+        return n - cannotRemove;
+    }
+};
