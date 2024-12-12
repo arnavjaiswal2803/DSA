d967aaf7399167832ea7dda69bb0e0504bc13c66
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-12 16:17:04 +0530

    Time: 114 ms (76.37%), Space: 127.9 MB (36.32%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -3,30 +3,33 @@ class Solution {
         return x >= 0 && x < m && y >= 0 && y < n;
     }
 
-    int dijstra(vector<vector<int>>& grid) {
+    int bfs01(vector<vector<int>>& grid) {
         const int INF = 1e9;
         int m = grid.size(), n = grid[0].size();
         vector<vector<int>> obstacleCount(m, vector<int>(n, INF));
-        priority_queue<pair<int, pair<int, int>>,
-            vector<pair<int, pair<int, int>>>, greater<>> minHeap;
+        deque<pair<int, pair<int, int>>> dq;
 
-        minHeap.push({0, {0, 0}});
+        dq.push_front({0, {0, 0}});
         obstacleCount[0][0] = 0;
 
         vector<int> dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
-        while (!minHeap.empty()) {
-            int obstacleRemoved = minHeap.top().first;
-            int x = minHeap.top().second.first, y = minHeap.top().second.second;
-            minHeap.pop();
+        while (!dq.empty()) {
+            int obstacleRemoved = dq.front().first;
+            int x = dq.front().second.first, y = dq.front().second.second;
+            dq.pop_front();
 
             if (x == m - 1 && y == n - 1) return obstacleRemoved;
 
             for (int i = 0; i < 4; i++) {
                 int newX = x + dx[i], newY = y + dy[i];
+
                 if (isValid(m, n, newX, newY) && 
                     obstacleRemoved + grid[newX][newY] < obstacleCount[newX][newY]) {
                         obstacleCount[newX][newY] = obstacleRemoved + grid[newX][newY];
-                        minHeap.push({obstacleCount[newX][newY], {newX, newY}});
+
+                        if (grid[newX][newY] == 0) 
+                            dq.push_front({obstacleCount[newX][newY], {newX, newY}});
+                        else dq.push_back({obstacleCount[newX][newY], {newX, newY}});
                     }
             }
         }
@@ -36,6 +39,6 @@ class Solution {
 
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
-        return dijstra(grid);
+        return bfs01(grid);
     }
 };
