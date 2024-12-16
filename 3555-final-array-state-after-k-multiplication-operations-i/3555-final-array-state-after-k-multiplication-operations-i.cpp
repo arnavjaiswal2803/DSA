5967cf7d66f3d00bca6b17872cd5129b3bdaa0f7
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-16 11:43:13 +0530

    Time: 3 ms (30.95%), Space: 27.8 MB (25.16%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -2,12 +2,23 @@ class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         int n = nums.size();
-        vector<int> ans(nums);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+        vector<int> ans(n);
+
+        for (int i = 0; i < n; i++) minHeap.push({nums[i], i});
+
         while (k--) {
-            int minIdx = 0;
-            for (int i = 1; i < n; i++) if (ans[i] < ans[minIdx]) minIdx = i;
-            ans[minIdx] *= multiplier;
+            int minn = minHeap.top().first, idx = minHeap.top().second;
+            minHeap.pop();
+            minHeap.push({minn * multiplier, idx});
         }
+
+        while (!minHeap.empty()) {
+            int idx = minHeap.top().second, elem = minHeap.top().first;
+            minHeap.pop();
+            ans[idx] = elem;
+        }
+
         return ans;
     }
 };
