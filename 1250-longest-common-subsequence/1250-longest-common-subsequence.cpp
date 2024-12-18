7d00adeaffa66df4b0198d6a0e5e6784e652efa5
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-18 17:20:17 +0530

    Time: 88 ms (5.05%), Space: 28.8 MB (7.12%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,28 +1,23 @@
 class Solution {
-public:
-    int longestCommonSubsequence(string s1, string s2) {
-        int n = s1.size(), m = s2.size();
-        vector<int> prev(m);
+    int lcs(string& s1, string& s2, int idx1, int idx2, vector<vector<int>>& dp) {
+        if (idx1 == 0 && idx2 == 0) return s1[idx1] == s2[idx2];
+        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
 
-        for (int idx1 = 0; idx1 < n; ++idx1) {
-            vector<int> curr(m);
-            for (int idx2 = 0; idx2 < m; ++idx2) {
-                if (idx1 == 0 && idx2 == 0) curr[idx2] = s1[idx1] == s2[idx2];
-                else {
-                    int equal = 0, unequal1 = 0, unequal2 = 0;
-                    if (idx1 > 0) unequal1 = prev[idx2];
-                    if (idx2 > 0) unequal2 = curr[idx2 - 1];
-                    if (s1[idx1] == s2[idx2]) {
-                        equal = 1;
-                        if (idx1 > 0 && idx2 > 0) equal += prev[idx2 - 1];
-                    }
-
-                    curr[idx2] = max({equal, unequal1, unequal2});
-                }
-            }
-            prev = curr;
+        int equal = 0, unequal1 = 0, unequal2 = 0;
+        if (s1[idx1] == s2[idx2]) {
+            equal = 1;
+            if (idx1 > 0 && idx2 > 0) equal += lcs(s1, s2, idx1 - 1, idx2 - 1, dp);
+        } else {
+            if (idx1 > 0) unequal1 = lcs(s1, s2, idx1 - 1, idx2, dp);
+            if (idx2 > 0) unequal2 = lcs(s1, s2, idx1, idx2 - 1, dp);
         }
 
-        return prev[m - 1];
+        return dp[idx1][idx2] = max({equal, unequal1, unequal2});
+    }
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return lcs(text1, text2, n - 1, m - 1, dp);
     }
 };
