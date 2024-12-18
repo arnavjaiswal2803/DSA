c3db75a1e59d2d8d7f6d37a3a050572df711e3e1
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-18 17:21:50 +0530

    Time: 47 ms (13.48%), Space: 26.7 MB (26.82%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     int longestCommonSubsequence(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<int> prev(m);
 
         for (int idx1 = 0; idx1 < n; ++idx1) {
+            vector<int> curr(m);
             for (int idx2 = 0; idx2 < m; ++idx2) {
-                if (idx1 == 0 && idx2 == 0) dp[idx1][idx2] = s1[idx1] == s2[idx2];
+                if (idx1 == 0 && idx2 == 0) curr[idx2] = s1[idx1] == s2[idx2];
                 else {
                     int equal = 0, unequal1 = 0, unequal2 = 0;
                     if (s1[idx1] == s2[idx2]) {
                         equal = 1;
-                        if (idx1 > 0 && idx2 > 0) equal += dp[idx1 - 1][idx2 - 1];
+                        if (idx1 > 0 && idx2 > 0) equal += prev[idx2 - 1];
                     } else {
-                        if (idx1 > 0) unequal1 = dp[idx1 - 1][idx2];
-                        if (idx2 > 0) unequal2 = dp[idx1][idx2 - 1];
+                        if (idx1 > 0) unequal1 = prev[idx2];
+                        if (idx2 > 0) unequal2 = curr[idx2 - 1];
                     }
 
-                    dp[idx1][idx2] = max({equal, unequal1, unequal2});
+                    curr[idx2] = max({equal, unequal1, unequal2});
                 }
             }
+            prev = curr;
         }
 
-        return dp[n - 1][m - 1];
+        return prev[m - 1];
     }
 };
