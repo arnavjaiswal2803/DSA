2dafbb6f334baa86dcc034049068708560651fab
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-18 15:45:27 +0530

    Time: 140 ms (5.05%), Space: 29.1 MB (7.12%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int lcs(string& s1, string& s2, int idx1, int idx2, vector<vector<int>>& dp) {
+        if (idx1 == 0 && idx2 == 0) return s1[idx1] == s2[idx2];
+        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
+
+        int equal = 0, unequal1 = 0, unequal2 = 0;
+        if (idx1 > 0) unequal1 = lcs(s1, s2, idx1 - 1, idx2, dp);
+        if (idx2 > 0) unequal2 = lcs(s1, s2, idx1, idx2 - 1, dp);
+        if (s1[idx1] == s2[idx2]) {
+            equal = 1;
+            if (idx1 > 0 && idx2 > 0) equal += lcs(s1, s2, idx1 - 1, idx2 - 1, dp);
+        }
+
+        return dp[idx1][idx2] = max({equal, unequal1, unequal2});
+    }
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return lcs(text1, text2, n - 1, m - 1, dp);
+    }
+};
