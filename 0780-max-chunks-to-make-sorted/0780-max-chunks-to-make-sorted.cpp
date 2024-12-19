3bbafa795f6ec3d18222fd4f8a3ad2e72ea16aa5
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-19 12:24:01 +0530

    Time: 0 ms (100%), Space: 9.3 MB (20.72%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -3,17 +3,20 @@ class Solution {
         if (startIdx == n) return 0;
 
         vector<bool> present(n, false);
+        int absentCount = 0;
         for (int endIdx = startIdx; endIdx < n; endIdx++) {
+            if (!present[endIdx]) absentCount++;
+            if (arr[endIdx] >= startIdx && arr[endIdx] <= endIdx &&
+                !present[arr[endIdx]])
+                    absentCount--;
             present[arr[endIdx]] = true;
 
-            bool areAllPresent = true;
-            for (int i = startIdx; i <= endIdx; i++) {
-                if (!present[i]) areAllPresent = false;
-            }
-            if (areAllPresent) return 1 + maxChunks(arr, n, endIdx + 1);
+            if (absentCount == 0)return 1 + maxChunks(arr, n, endIdx + 1);
         }
+
         return -1e8;
     }
+
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int n = arr.size();
