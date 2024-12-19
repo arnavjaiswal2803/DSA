9ea691f71a0a83b2a6990406da2aa10d863cd855
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-19 12:29:38 +0530

    Time: 0 ms (100%), Space: 9.1 MB (35.22%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -1,25 +1,27 @@
 class Solution {
-    int maxChunks(vector<int>& arr, int n, int startIdx) {
-        if (startIdx == n) return 0;
-
-        vector<bool> present(n, false);
-        int absentCount = 0;
-        for (int endIdx = startIdx; endIdx < n; endIdx++) {
-            if (!present[endIdx]) absentCount++;
-            if (arr[endIdx] >= startIdx && arr[endIdx] <= endIdx &&
-                !present[arr[endIdx]])
-                    absentCount--;
-            present[arr[endIdx]] = true;
-
-            if (absentCount == 0)return 1 + maxChunks(arr, n, endIdx + 1);
-        }
-
-        return -1e8;
-    }
-
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int n = arr.size();
-        return maxChunks(arr, n, 0);
+        int n = arr.size(), chunks = 0;
+
+        for (int startIdx = 0; startIdx < n; startIdx++) {
+            vector<bool> present(n, false);
+            int absentCount = 0;
+            
+            for (int endIdx = startIdx; endIdx < n; endIdx++) {
+                if (!present[endIdx]) absentCount++;
+                if (arr[endIdx] >= startIdx && arr[endIdx] <= endIdx &&
+                    !present[arr[endIdx]])
+                        absentCount--;
+                present[arr[endIdx]] = true;
+
+                if (absentCount == 0) {
+                    chunks++;
+                    startIdx = endIdx;
+                    break;
+                }
+            }
+        }
+
+        return chunks;
     }
 };
