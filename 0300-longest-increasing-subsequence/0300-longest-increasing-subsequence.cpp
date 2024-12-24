7635ed45a2a1317d84f34ddedd07da0c3a29fdf3
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-24 18:03:26 +0530

    Time: 63 ms (59.88%), Space: 14.2 MB (45.57%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ahead(n + 1);
-
-        for (int idx = n; idx >= 0; --idx) {
-            vector<int> curr(n + 1);
-            for (int prevIdx = idx - 1; prevIdx >= -1; --prevIdx) {
-                if (idx == n) curr[prevIdx + 1] = 0;
-                else {
-                    int exclude = ahead[prevIdx + 1], include = 0;
-                    if (prevIdx == -1 || nums[idx] > nums[prevIdx]) 
-                        include = 1 + ahead[idx + 1];
-
-                    curr[prevIdx + 1] = max(include, exclude);
-                }
-            }
-            ahead = curr;
-        }
+        int n = nums.size(), maxLen = 1;
+        vector<int> dp(n, 1);
         
-        return ahead[0];
+        for (int idx = 0; idx < n; idx++) {
+            for (int prevIdx = 0; prevIdx < idx; prevIdx++) {
+                if (nums[prevIdx] < nums[idx]) 
+                    dp[idx] = max(dp[idx], 1 + dp[prevIdx]);
+            }
+            maxLen = max(maxLen, dp[idx]);
+        }
+
+        return maxLen;
     }
 };
