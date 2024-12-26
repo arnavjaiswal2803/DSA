b218fb829106d4814094765ed96dc98f5adc908c
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-26 11:55:47 +0530

    Time: 219 ms (40.79%), Space: 54.4 MB (17.28%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,7 +1,11 @@
 class Solution {
     int ways(vector<int>& nums, int target, int idx, 
         map<pair<int, int>, int>& dp) {
-            if (idx < 0) return target == 0;
+            if (idx == 0) {
+                if (target + nums[idx] == 0 && target - nums[idx] == 0) return 2;
+                if (target + nums[idx] == 0 || target - nums[idx] == 0) return 1;
+                return 0;
+            };
             if (dp.find({idx, target}) != dp.end()) return dp[{idx, target}];
 
             int add = ways(nums, target - nums[idx], idx - 1, dp);
