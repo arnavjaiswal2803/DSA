b8acc2b9496ed5fb59adcf1e47163d511388fdf7
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-26 13:29:56 +0530

    Time: 8 ms (67.51%), Space: 24.7 MB (30.56%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,4 @@
 class Solution {
-    int ways(vector<int>& nums, int idx, int currSum, int target, int totalSum,
-             vector<vector<int>>& dp) {
-        if (idx == 0) {
-            if (currSum + nums[idx] == target && currSum - nums[idx] == target)
-                return 2;
-            if (currSum + nums[idx] == target || currSum - nums[idx] == target)
-                return 1;
-            return 0;
-        }
-        if (dp[idx][currSum + totalSum] != INT_MIN)
-            return dp[idx][currSum + totalSum];
-
-        int add =
-            ways(nums, idx - 1, currSum + nums[idx], target, totalSum, dp);
-        int sub =
-            ways(nums, idx - 1, currSum - nums[idx], target, totalSum, dp);
-        return dp[idx][currSum + totalSum] = add + sub;
-    }
-
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size(), totalSum = accumulate(nums.begin(), nums.end(), 0);
