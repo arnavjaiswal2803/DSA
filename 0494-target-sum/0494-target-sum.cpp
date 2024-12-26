8cd6e3e0a2cdf5fee29126fa1ef077a74201a561
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-26 13:29:31 +0530

    Time: 17 ms (56.19%), Space: 24.7 MB (30.32%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -21,22 +21,24 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size(), totalSum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<int>> dp(n, vector<int>(2 * totalSum + 1, 0));
+        vector<int> prev(2 * totalSum + 1, 0);
 
-        dp[0][nums[0] + totalSum] = 1;
-        dp[0][-nums[0] + totalSum] += 1;
+        prev[nums[0] + totalSum] = 1;
+        prev[-nums[0] + totalSum] += 1;
 
         for (int idx = 1; idx < n; idx++) {
+            vector<int> curr(2 * totalSum + 1, 0);
             for (int currSum = -totalSum; currSum <= totalSum; currSum++) {
-                if (dp[idx - 1][currSum + totalSum] > 0) {
-                    dp[idx][currSum + nums[idx] + totalSum] +=
-                        dp[idx - 1][currSum + totalSum];
-                    dp[idx][currSum - nums[idx] + totalSum] +=
-                        dp[idx - 1][currSum + totalSum];
+                if (prev[currSum + totalSum] > 0) {
+                    curr[currSum + nums[idx] + totalSum] +=
+                        prev[currSum + totalSum];
+                    curr[currSum - nums[idx] + totalSum] +=
+                        prev[currSum + totalSum];
                 }
             }
+            prev = curr;
         }
 
-        return abs(target) > totalSum ? 0 : dp[n - 1][target + totalSum];
+        return abs(target) > totalSum ? 0 : prev[target + totalSum];
     }
 };
