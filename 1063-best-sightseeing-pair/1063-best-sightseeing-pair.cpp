31932b0dc610957ee7da7b7ffecd3f61a23840b3
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-29 15:55:34 +0530

    Time: 1 ms (35.48%), Space: 45.5 MB (78.09%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        int n = values.size();
-        // Initialize an array to store the maximum left scores up to each
-        // index.
-        vector<int> maxLeftScore(n);
-        // The left score at the first index is just the value of the first
-        // element.
-        maxLeftScore[0] = values[0];
-
-        int maxScore = 0;
-
+        int n = values.size(), maxLeft = values[0], maxScore = -1e9;
+        
         for (int i = 1; i < n; i++) {
-            int currentRightScore = values[i] - i;
-            // Update the maximum score by combining the best left score so far
-            // with the current right score.
-            maxScore = max(maxScore, maxLeftScore[i - 1] + currentRightScore);
-
-            int currentLeftScore = values[i] + i;
-            // Update the maximum left score up to the current index.
-            maxLeftScore[i] = max(maxLeftScore[i - 1], currentLeftScore);
+            int currScore = maxLeft + values[i] - i;
+            maxScore = max(maxScore, currScore);
+            maxLeft = max(maxLeft, values[i] + i);
         }
 
         return maxScore;
