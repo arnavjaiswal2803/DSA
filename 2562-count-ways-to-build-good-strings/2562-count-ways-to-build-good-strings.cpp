937c3a2e278d318ffab272d44162fc7654614cd7
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-30 12:36:51 +0530

    Time: 12 ms (12.33%), Space: 20.9 MB (25.44%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -3,21 +3,8 @@ class Solution {
 
     int count(int low, int high, int zero, int one, int len, vector<int>& dp) {
         if (dp[len] != -1) return dp[len];
-        if (len >= low) {
-            int cnt = 1;
-            if (len + zero <= high) 
-                cnt = (cnt + 
-                    (count(low, high, zero, one, len + zero, dp) % MOD)
-                ) % MOD;
-            if (len + one <= high) 
-                cnt = (cnt + 
-                    (count(low, high, zero, one, len + one, dp) % MOD)
-                ) % MOD;
-
-            return dp[len] = cnt;
-        }
-
-        int cnt = 0;
+        
+        int cnt = len >= low ? 1 : 0;
         if (len + zero <= high) 
             cnt = (cnt + 
                 (count(low, high, zero, one, len + zero, dp) % MOD)
