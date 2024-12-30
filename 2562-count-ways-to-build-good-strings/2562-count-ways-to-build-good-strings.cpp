fb60df4ec65080265bd66305beae307be047a5f2
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-30 12:43:14 +0530

    Time: 3 ms (96.09%), Space: 11.8 MB (62.82%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -1,24 +1,16 @@
 class Solution {
-    const int MOD = 1e9 + 7;
-
-    int count(int low, int high, int zero, int one, int len, vector<int>& dp) {
-        if (dp[len] != -1) return dp[len];
-        
-        int cnt = len >= low ? 1 : 0;
-        if (len + zero <= high) 
-            cnt = (cnt + 
-                (count(low, high, zero, one, len + zero, dp) % MOD)
-            ) % MOD;
-        if (len + one <= high) 
-            cnt = (cnt + 
-                (count(low, high, zero, one, len + one, dp) % MOD)
-            ) % MOD;
-        return dp[len] = cnt;
-    }
-
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int> dp(high + 1, -1);
-        return count(low, high, zero, one, 0, dp);
+        const int MOD = 1e9 + 7;
+        vector<int> dp(high + 1, 0);
+
+        for (int len = high; len >= 0; len--) {
+            int cnt = len >= low ? 1 : 0;
+            if (len + zero <= high) cnt = (cnt + dp[len + zero]) % MOD;
+            if (len + one <= high) cnt = (cnt + dp[len + one]) % MOD;
+            dp[len] = cnt;
+        }
+
+        return dp[0];
     }
 };
