ebf2953962cd8200e3d598271c53364e31640666
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-31 13:21:35 +0530

    Time: 19 ms (5.07%), Space: 23.1 MB (5.11%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,21 +1,23 @@
 class Solution {
-    int minCost(vector<int>& days, vector<int>& costs, int n, int idx, 
-        int validUpto, vector<vector<int>>& dp) {
-            if (idx == n) return 0;
-            if (dp[idx][validUpto] != -1) return dp[idx][validUpto];
-
-            if (days[idx] <= validUpto) 
-                return minCost(days, costs, n, idx + 1, validUpto, dp);
-            return dp[idx][validUpto] = min({
-                costs[0] + minCost(days, costs, n, idx + 1, days[idx], dp), 
-                costs[1] + minCost(days, costs, n, idx + 1, days[idx] + 6, dp), 
-                costs[2] + minCost(days, costs, n, idx + 1, days[idx] + 29, dp)
-            });
-        }
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int n = days.size();
-        vector<vector<int>> dp(n + 1, vector<int>(days[n - 1] + 30, -1));
-        return minCost(days, costs, n, 0, 0, dp);
+        int n = days.size(), maxValidity = 365 + 30;
+        vector<vector<int>> dp(n + 1, vector<int>(maxValidity + 1, 0));
+
+        for (int idx = n - 1; idx >= 0; --idx) {
+            for (int validUpto = maxValidity; validUpto >= 0; --validUpto) {
+                if (days[idx] <= validUpto) 
+                    dp[idx][validUpto] = dp[idx + 1][validUpto];
+                else {
+                    dp[idx][validUpto] = min({
+                        costs[0] + dp[idx + 1][days[idx]], 
+                        costs[1] + dp[idx + 1][days[idx] + 6], 
+                        costs[2] + dp[idx + 1][days[idx] + 29]
+                    });
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 }; 
