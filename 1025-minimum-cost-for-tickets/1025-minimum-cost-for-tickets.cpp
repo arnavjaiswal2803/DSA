7c1cb6ac548ed856b0d96b3ac976545632fc0786
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2024-12-31 13:24:58 +0530

    Time: 20 ms (5.07%), Space: 20.6 MB (7.43%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size(), maxValidity = days[n - 1] + 30;
-        vector<vector<int>> dp(n + 1, vector<int>(maxValidity + 1, 0));
+        vector<int> next(maxValidity + 1, 0);
 
         for (int idx = n - 1; idx >= 0; --idx) {
+            vector<int> curr(maxValidity + 1, 0);
             for (int validUpto = maxValidity; validUpto >= 0; --validUpto) {
                 if (days[idx] <= validUpto) 
-                    dp[idx][validUpto] = dp[idx + 1][validUpto];
+                    curr[validUpto] = next[validUpto];
                 else {
-                    dp[idx][validUpto] = min({
-                        costs[0] + dp[idx + 1][days[idx]], 
-                        costs[1] + dp[idx + 1][days[idx] + 6], 
-                        costs[2] + dp[idx + 1][days[idx] + 29]
+                    curr[validUpto] = min({
+                        costs[0] + next[days[idx]], 
+                        costs[1] + next[days[idx] + 6], 
+                        costs[2] + next[days[idx] + 29]
                     });
                 }
             }
+            next = curr;
         }
 
-        return dp[0][0];
+        return next[0];
     }
 }; 
