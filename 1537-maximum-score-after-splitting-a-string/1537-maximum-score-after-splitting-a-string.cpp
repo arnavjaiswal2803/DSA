d67fca218dfcb6520bc73150563ae83294ca5f56
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-01-01 14:14:39 +0530

    Time: 0 ms (100%), Space: 8.9 MB (18.99%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n = s.size(), maxScore = 0;
-        vector<int> leftZeroCnt(n), rightOneCnt(n);
+        int n = s.size(), maxScore = 0, leftZeroCnt = 0, rightOneCnt = 0;
 
-        for (int i = 0; i < n; i++) {
-            leftZeroCnt[i] = s[i] == '0' ? 1 : 0;
-            rightOneCnt[n - 1 - i] = s[n - 1 - i] == '1' ? 1 : 0;
-
-            if (i > 0) {
-                leftZeroCnt[i] += leftZeroCnt[i - 1];
-                rightOneCnt[n - 1 - i] += rightOneCnt[n - i];
-            }
-        }
-
+        for (int i = n - 1; i >= 1; i--) rightOneCnt += s[i] == '1';
         for (int i = 1; i < n; i++) {
-            maxScore = max(maxScore, leftZeroCnt[i - 1] + rightOneCnt[i]);
+            leftZeroCnt += s[i - 1] == '0';
+            maxScore = max(maxScore, leftZeroCnt + rightOneCnt);
+            rightOneCnt -= s[i] == '1';
         }
 
         return maxScore;
