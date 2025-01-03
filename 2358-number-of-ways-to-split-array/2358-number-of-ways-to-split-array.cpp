92839ab20d6ca49cff043250d06a79bb0d741bc4
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-01-03 11:32:07 +0530

    Time: 0 ms (100%), Space: 89.4 MB (44.2%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
         int n = nums.size(), validSplits = 0;
-        vector<long long> prefixSum(n);
+        long currSum = 0, totalSum = accumulate(nums.begin(), nums.end(), (long)0);
 
-        for (int i = 0; i < n; i++) {
-            prefixSum[i] = nums[i];
-            if (i > 0) prefixSum[i] += prefixSum[i - 1];
+        for (int i = 0; i < n - 1; i++) {
+            currSum += nums[i];
+            if (2 * currSum >= totalSum) validSplits++;
         }
-
-        for (int i = 0; i < n - 1; i++) 
-            if (prefixSum[i] >= prefixSum[n - 1] - prefixSum[i]) validSplits++;
 
         return validSplits;
     }
