247a5c04cf7a7ef5e1405b18e6fc904268c68da0
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-01-06 17:40:28 +0530

    Time: 0 ms (100%), Space: 12.5 MB (24.96%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,11 +2,22 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n = boxes.size();
-        vector<int> ans(n, 0);
+        vector<int> ans(n), movingRightPrefixSum(n), movingLeftPrefixSum(n);
         
+        int oneCount = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = i - 1; j >= 0; j--) if (boxes[j] == '1') ans[i] += i - j;
-            for (int j = i + 1; j < n; j++) if (boxes[j] == '1') ans[i] += j - i;
+            movingRightPrefixSum[i] = oneCount;
+            if (i > 0) movingRightPrefixSum[i] += movingRightPrefixSum[i - 1];
+            if (boxes[i] == '1') oneCount++;
+        }
+
+        oneCount = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            movingLeftPrefixSum[i] = oneCount;
+            if (i < n - 1) movingLeftPrefixSum[i] += movingLeftPrefixSum[i + 1];
+            if (boxes[i] == '1') oneCount++;
+
+            ans[i] = movingRightPrefixSum[i] + movingLeftPrefixSum[i];
         }
 
         return ans;
