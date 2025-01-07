f6feff0e11ceb98d99aca7a9b6868a6fc7a0daf9
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-01-07 13:59:11 +0530

    Time: 15 ms (8.3%), Space: 29.3 MB (5.35%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,18 +1,75 @@
+class TrieNode {
+    TrieNode* links[26] = {0};
+    int frequency;
+
+    public: 
+
+    TrieNode() {
+        frequency = 0;
+    }
+
+    bool containsKey(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void put(char ch, TrieNode* trieNode) {
+        links[ch - 'a'] = trieNode;
+    }
+
+    TrieNode* get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void incrementFrequency() {
+        frequency++;
+    }
+
+    int getFrequency() {
+        return frequency;
+    }
+};
+
+class Trie {
+    TrieNode* root;
+
+    public:
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insertSuffixesOfWord(string& word) {
+        int n = word.size();
+        for (int i = 0; i < n; i++) {
+            TrieNode* node = root;
+
+            for (int j = i; j < n; j++) {
+                if (!node->containsKey(word[j])) node->put(word[j], new TrieNode());
+                node = node->get(word[j]);
+                node->incrementFrequency();
+            }
+        }
+    }
+
+    bool isSubstring(string& word) {
+        int n = word.size();
+        TrieNode* node = root;
+        for (int i = 0; i < n; i++) {
+            node = node->get(word[i]);
+        }
+        return node->getFrequency() > 1;
+    }
+};
+
 class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
         int n = words.size();
+        Trie* trie = new Trie();
         vector<string> ans;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j == i) continue;
-                if (words[j].find(words[i]) != string::npos) {
-                    ans.push_back(words[i]);
-                    break;
-                }
-            }
-        }
+        for (string& word : words) trie->insertSuffixesOfWord(word);
+        for (string& word : words) if (trie->isSubstring(word)) ans.push_back(word);
 
         return ans;
     }
