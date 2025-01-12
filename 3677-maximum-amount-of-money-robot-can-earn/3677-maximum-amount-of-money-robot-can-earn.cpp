8768eb26c8ced285d0908f725aaa73f9bd0c1dc5
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-01-13 00:52:50 +0530

    Time: 489 ms (0%), Space: 251.4 MB (0%) - LeetHub

--- 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
@@ -1,31 +1,38 @@
 class Solution {
-    int getMaxProfit(vector<vector<int>>& coins, int row, int col,
-                     int neutralize, vector<vector<vector<int>>>& dp) {
-        if (row < 0 || col < 0)
-            return -1e9;
-        if (row == 0 && col == 0) {
-            return coins[row][col] >= 0 || neutralize == 0 ? coins[row][col]
-                                                           : 0;
-        }
-        if (dp[row][col][neutralize] != -1e9+7) return dp[row][col][neutralize];
-
-        if (coins[row][col] >= 0 || neutralize == 0) {
-            return dp[row][col][neutralize] = coins[row][col] +
-                   max(getMaxProfit(coins, row - 1, col, neutralize, dp),
-                       getMaxProfit(coins, row, col - 1, neutralize, dp));
-        }
-        return dp[row][col][neutralize] = max(coins[row][col] +
-                       max(getMaxProfit(coins, row - 1, col, neutralize, dp),
-                           getMaxProfit(coins, row, col - 1, neutralize, dp)),
-                   max(getMaxProfit(coins, row - 1, col, neutralize - 1, dp),
-                       getMaxProfit(coins, row, col - 1, neutralize - 1, dp)));
-    }
-
-public:
+    public:
     int maximumAmount(vector<vector<int>>& coins) {
         int m = coins.size(), n = coins[0].size();
-        vector<vector<vector<int>>> 
-            dp(m, vector<vector<int>>(n, vector<int>(3, -1e9+7)));
-        return getMaxProfit(coins, m - 1, n - 1, 2, dp);
+        vector<vector<vector<int>>> dp(
+            m + 1, vector<vector<int>>(n + 1, vector<int>(3, -1e9 + 7)));
+
+        for (int row = 1; row <= m; row++) {
+            for (int col = 1; col <= n; col++) {
+                for (int neutralize = 0; neutralize <= 2; neutralize++) {
+
+                    if (row == 1 && col == 1) {
+                        dp[row][col][neutralize] =
+                            coins[row - 1][col - 1] >= 0 || neutralize == 0
+                                ? coins[row - 1][col - 1]
+                                : 0;
+                    } else {
+                        if (coins[row - 1][col - 1] >= 0 || neutralize == 0) {
+                            dp[row][col][neutralize] =
+                                coins[row - 1][col - 1] +
+                                max(dp[row - 1][col][neutralize],
+                                    dp[row][col - 1][neutralize]);
+                        } else {
+                            dp[row][col][neutralize] =
+                                max(coins[row - 1][col - 1] +
+                                        max(dp[row - 1][col][neutralize],
+                                            dp[row][col - 1][neutralize]),
+                                    max(dp[row - 1][col][neutralize - 1],
+                                        dp[row][col - 1][neutralize - 1]));
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[m][n][2];
     }
 };
