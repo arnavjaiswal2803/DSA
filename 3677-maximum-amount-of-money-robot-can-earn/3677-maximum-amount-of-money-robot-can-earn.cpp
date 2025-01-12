86abdf718a22284a6ac63f7396ab1077d2121cc3
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-01-13 00:56:08 +0530

    Time: 520 ms (0%), Space: 250.6 MB (0%) - LeetHub

--- 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
@@ -2,37 +2,41 @@ class Solution {
     public:
     int maximumAmount(vector<vector<int>>& coins) {
         int m = coins.size(), n = coins[0].size();
-        vector<vector<vector<int>>> dp(
-            m + 1, vector<vector<int>>(n + 1, vector<int>(3, -1e9 + 7)));
+        vector<vector<int>> prev(n + 1, vector<int>(3, -1e9 + 7));
 
         for (int row = 1; row <= m; row++) {
+            vector<vector<int>> curr(n + 1, vector<int>(3, -1e9 + 7));
+
             for (int col = 1; col <= n; col++) {
                 for (int neutralize = 0; neutralize <= 2; neutralize++) {
 
                     if (row == 1 && col == 1) {
-                        dp[row][col][neutralize] =
+                        curr[col][neutralize] =
                             coins[row - 1][col - 1] >= 0 || neutralize == 0
                                 ? coins[row - 1][col - 1]
                                 : 0;
                     } else {
                         if (coins[row - 1][col - 1] >= 0 || neutralize == 0) {
-                            dp[row][col][neutralize] =
+                            curr[col][neutralize] =
                                 coins[row - 1][col - 1] +
-                                max(dp[row - 1][col][neutralize],
-                                    dp[row][col - 1][neutralize]);
+                                max(prev[col][neutralize],
+                                    curr[col - 1][neutralize]);
                         } else {
-                            dp[row][col][neutralize] =
+                            curr[col][neutralize] =
                                 max(coins[row - 1][col - 1] +
-                                        max(dp[row - 1][col][neutralize],
-                                            dp[row][col - 1][neutralize]),
-                                    max(dp[row - 1][col][neutralize - 1],
-                                        dp[row][col - 1][neutralize - 1]));
+                                        max(prev[col][neutralize],
+                                            curr[col - 1][neutralize]),
+                                    max(prev[col][neutralize - 1],
+                                        curr[col - 1][neutralize - 1]));
                         }
                     }
+
                 }
             }
+
+            prev = curr;
         }
 
-        return dp[m][n][2];
+        return prev[n][2];
     }
 };
