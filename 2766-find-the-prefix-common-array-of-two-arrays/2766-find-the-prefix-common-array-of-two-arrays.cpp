a2e6131beb0e9536ea6eb7b1585ddaea7e07390e
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-01-14 14:19:34 +0530

    Time: 2 ms (78.4%), Space: 85.6 MB (92.22%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,12 +1,22 @@
 class Solution {
+    bool getBit(long int mask, int pos) {
+        return (mask & ((long int)1 << pos)) != 0;
+    }
+
+    void setBit(long int &mask, int pos) {
+        mask |= ((long int)1 << pos);
+    }
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size(), count = 0;
-        vector<int> ans(n), freq(n + 1, 0);
+        long int mask = 0;
+        vector<int> ans(n);
 
         for (int i = 0; i < n; i++) {
-            if (++freq[A[i]] == 2) count++;
-            if (++freq[B[i]] == 2) count++;
+            if (getBit(mask, A[i])) count++;
+            setBit(mask, A[i]);
+            if (getBit(mask, B[i])) count++;
+            setBit(mask, B[i]);
             ans[i] = count;
         }
 
