0640353c4e7cc2e310e7840499790d73e4d5048c
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-01-17 17:32:35 +0530

    Time: 0 ms (100%), Space: 263.9 MB (74.77%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,15 +1,8 @@
 class Solution {
-    bool isPossible(vector<int>& derived, int first) {
-        int curr = first, next;
-        for (int &num : derived) {
-            if (num == 1) next = curr == 1 ? 0 : 1;
-            else next = curr == 1 ? 1 : 0;
-            curr = next;
-        }
-        return next == first;
-    }
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        return isPossible(derived, 0) || isPossible(derived, 1);
+        int xorr = 0;
+        for (int &num : derived) xorr ^= num;
+        return xorr == 0;
     }
 };
