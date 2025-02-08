0caa2c8cb8492a318cefae841a4d10993a0ee4e9
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-08 22:06:31 +0530

    Time: 114 ms (59.56%), Space: 155.8 MB (60.81%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -2,33 +2,24 @@ class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
         int n = queries.size();
-        vector<int> result(n);
-        unordered_map<int, int> colorMap, ballMap;
+        vector<int> ans(n);
+        unordered_map<int, int> ballColorMap, colorFreq;
 
-        // Iterate through queries
         for (int i = 0; i < n; i++) {
-            // Extract ball label and color from query
             int ball = queries[i][0], color = queries[i][1];
 
-            // Check if ball is already covered
-            if (ballMap.find(ball) != ballMap.end()) {
-                // Decrement count of the previous color on the ball
-                int prevColor = ballMap[ball];
-                colorMap[prevColor]--;
-
-                // If there are no balls with previous color left, remove color
-                // from color map
-                if (colorMap[prevColor] == 0) colorMap.erase(prevColor);
+            if (ballColorMap.find(ball) != ballColorMap.end()) {
+                int prevColor = ballColorMap[ball];
+                colorFreq[prevColor]--;
+                if (colorFreq[prevColor] == 0) colorFreq.erase(prevColor);
             }
-            // Set color of ball to the new color
-            ballMap[ball] = color;
 
-            // Increment the count of the new color
-            colorMap[color]++;
+            ballColorMap[ball] = color;
+            colorFreq[color]++; 
 
-            result[i] = colorMap.size();
+            ans[i] = colorFreq.size();
         }
 
-        return result;
+        return ans;
     }
 };
