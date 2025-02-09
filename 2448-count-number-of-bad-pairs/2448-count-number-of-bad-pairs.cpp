cdfa79ef44ad2a3f252c9d603f28ac89b5f4002e
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-10 01:33:05 +0530

    Time: 146 ms (5.7%), Space: 89.5 MB (35.73%) - LeetHub

---
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        int n = nums.size();
+        long long totalPairs = ((long long)n * (long long)(n - 1)) / 2, goodPairs = 0;
+        map<int, long long> hash;
+
+        for (int i = 0; i < n; i++) {
+            if (hash.find(nums[i] - i) != hash.end()) {
+                goodPairs += hash[nums[i] - i];
+            }
+            hash[nums[i] - i]++;
+        }
+
+        return totalPairs - goodPairs;
+    }
+};
