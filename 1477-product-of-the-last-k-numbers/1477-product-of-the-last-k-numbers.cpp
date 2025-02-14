bc9239236b95656937b9a528d6b142f0893afec4
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-15 00:25:02 +0530

    Time: 12 ms (91.95%), Space: 78.2 MB (29.16%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,20 +1,28 @@
 class ProductOfNumbers {
-    vector<int> nums;
-    int size;
+    int size, lastZeroIdx;
+    vector<int> prefixProd;
 public:
     ProductOfNumbers() {
         size = 0;
+        lastZeroIdx = -1;
     }
     
     void add(int num) {
         size++;
-        nums.push_back(num);
+
+        prefixProd.push_back(num);
+        if (size > 1 && prefixProd[size - 2] != 0) {
+            prefixProd[size - 1] *= prefixProd[size - 2];
+        }
+        
+        if (num == 0) lastZeroIdx = size - 1;
     }
     
     int getProduct(int k) {
-        int product = 1;
-        for (int i = size - 1; i >= size - k; i--) product *= nums[i];
-        return product;
+        int idx = size - k;
+        if (idx <= lastZeroIdx) return 0;
+        if (idx == 0 || prefixProd[idx - 1] == 0) return prefixProd[size - 1];
+        return prefixProd[size - 1] / prefixProd[idx - 1];
     }
 };
 
