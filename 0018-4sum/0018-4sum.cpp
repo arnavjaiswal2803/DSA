073095c3f3bbf99e68c22e994293c4e9538b1955
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-15 15:42:44 +0530

    Time: 24 ms (41.19%), Space: 17.4 MB (85.81%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,30 +1,41 @@
 typedef long l;
+
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
         if (n < 4) return {};
-        set<vector<int>> quadSet;
+        vector<vector<int>> quads;
+
+        sort (nums.begin(), nums.end());
 
         for (int a = 0; a < n; a++) {
+            if (a > 0 && nums[a] == nums[a - 1]) continue;
+
+            bool bIterated = false;
             for (int b = a + 1; b < n; b++) {
-                unordered_set<int> hash;
-                for (int c = b + 1; c < n; c++) {
-                    long toFind = (l)target - ((l)nums[a] + (l)nums[b] + (l)nums[c]);
-                    if (toFind > INT_MAX || toFind < INT_MIN) continue;
+                if (bIterated && nums[b] == nums[b - 1]) continue;
 
-                    if (hash.find(toFind) != hash.end()) {
-                        vector<int> quad = {nums[a], nums[b], nums[c], (int)toFind};
-                        sort(quad.begin(), quad.end());
-                        quadSet.insert(quad);
+                int c = b + 1, d = n - 1;
+                while (c < d) {
+                    long sum = (l)nums[a] + (l)nums[b] + (l)nums[c] + (l)nums[d];
+
+                    if (sum < target) c++;
+                    else if (sum > target) d--;
+                    else {
+                        quads.push_back({nums[a], nums[b], nums[c], nums[d]});
+                        c++;
+                        d--;
+
+                        while (c < d && nums[c] == nums[c - 1]) c++;
+                        while (c < d && nums[d] == nums[d + 1]) d--;
                     }
-
-                    hash.insert(nums[c]);
                 }
+
+                bIterated = true;
             }
         }
 
-        vector<vector<int>> quads(quadSet.begin(), quadSet.end());
         return quads;
     }
 };
