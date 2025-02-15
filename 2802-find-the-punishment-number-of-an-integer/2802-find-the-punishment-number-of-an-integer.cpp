d80057522b2275999ae97883d96e0d86c1092cff
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-15 12:33:28 +0530

    Time: 26 ms (74.31%), Space: 9.1 MB (71.28%) - LeetHub

---
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    bool canPartition(string &numStr, int idx, int len, int sum, int target) {
+        if (idx == len) return sum == target;
+
+        int num = 0;
+        for (int i = idx; i < len; i++) {
+            num = (num * 10) + (numStr[i] - '0');
+            if (canPartition(numStr, i + 1, len, sum + num, target)) return true;
+        }
+        return false;
+    }
+public:
+    int punishmentNumber(int n) {
+        int punishmentNo = 0;
+        for (int i = 1; i <= n; i++) {
+            string numStr = to_string(i * i);
+            int len = numStr.size();
+            if (canPartition(numStr, 0, len, 0, i)) punishmentNo += (i * i);
+        }
+        return punishmentNo;
+    }
+};
