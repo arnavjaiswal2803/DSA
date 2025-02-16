1e2bc22cd5721130a93700580916ee778e7f79cf
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-16 11:48:37 +0530

    Time: 96 ms (90%), Space: 182.2 MB (23.33%) - LeetHub

--- 3763-separate-squares-i/3763-separate-squares-i.cpp
+++ 3763-separate-squares-i/3763-separate-squares-i.cpp
@@ -1,6 +1,25 @@
 class Solution {
+    double getLowerMinusUpperArea(vector<vector<int>>& squares, double mid) {
+        double lowerArea = 0, upperArea = 0;
+
+        for (auto &square : squares) {
+            double lowerY = square[1], sideLen = square[2], upperY = lowerY + sideLen;
+
+            if (upperY <= mid) {
+                lowerArea += (sideLen * sideLen);
+            } else if (lowerY >= mid) {
+                upperArea += (sideLen * sideLen);
+            } else {
+                lowerArea += (sideLen * (mid - lowerY));
+                upperArea += (sideLen * (upperY - mid));
+            }
+        }
+
+        return lowerArea - upperArea;
+    }
 public:
     double separateSquares(vector<vector<int>>& squares) {
+        int n = squares.size();
         double low = INT_MAX, high = 0;
 
         for (auto &sq : squares) {
@@ -11,23 +30,8 @@ public:
         while (high - low > 1e-5) {
             double mid = (low + high) / 2;
 
-            double lowerArea = 0, upperArea = 0;
-
-            for (auto &square : squares) {
-                double lowerY = square[1], sideLen = square[2], 
-                    upperY = lowerY + sideLen;
-
-                if (upperY <= mid) {
-                    lowerArea += (sideLen * sideLen);
-                } else if (lowerY >= mid) {
-                    upperArea += (sideLen * sideLen);
-                } else {
-                    lowerArea += (sideLen * (mid - lowerY));
-                    upperArea += (sideLen * (upperY - mid));
-                }
-            }
-
-            upperArea > lowerArea ? low = mid : high = mid;
+            if (getLowerMinusUpperArea(squares, mid) >= 0) high = mid;
+            else low = mid;
         }
 
         return high;
