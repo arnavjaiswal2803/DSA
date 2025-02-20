51a7fe040975f0c6dbf62d504641404b6844c808
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-20 21:40:51 +0530

    Time: 0 ms (100%), Space: 9.5 MB (7.19%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,49 +1,32 @@
 class Solution {
-    bool getNum(int idx, int n, string& pattern, string& curr, string& ans, 
-        bitset<10>& isUsed) {
-            if (idx == n + 1) {
-                ans = curr;
-                return true;
-            }
+    bool getNum(int idx, int n, string& pattern, string curr, string& ans,
+                bitset<10>& isUsed) {
+        if (idx == n + 1) {
+            ans = curr;
+            return true;
+        }
 
-            for (int i = 1; i <= 9; i++) {
-                if (idx == 0) {
-                    isUsed[i] = true;
-                    curr += to_string(i);
-                    if (getNum(idx + 1, n, pattern, curr, ans, isUsed)) 
-                        return true;
-                    curr.pop_back();
-                    isUsed[i] = false;
-                } else {
-                    if (pattern[idx - 1] == 'I') {
-                        if (!isUsed[i] && i > (curr[idx - 1] - '0')) {
-                            isUsed[i] = true;
-                            curr += to_string(i);
-                            if (getNum(idx + 1, n, pattern, curr, ans, isUsed)) 
-                                return true;
-                            curr.pop_back();
-                            isUsed[i] = false;
-                        } 
-                    } else {
-                        if (!isUsed[i] && i < (curr[idx - 1] - '0')) {
-                            isUsed[i] = true;
-                            curr += to_string(i);
-                            if (getNum(idx + 1, n, pattern, curr, ans, isUsed))
-                                return true;
-                            curr.pop_back();
-                            isUsed[i] = false;
-                        }
-                    }
-                }
+        for (int i = 1; i <= 9; i++) {
+            if (!isUsed[i] &&
+                (idx == 0 ||
+                 (pattern[idx - 1] == 'I' && i > curr[idx - 1] - '0') ||
+                 (pattern[idx - 1] == 'D' && i < curr[idx - 1] - '0'))) {
+
+                isUsed[i] = true;
+                if (getNum(idx + 1, n, pattern, curr + to_string(i), ans, isUsed))
+                    return true;
+                isUsed[i] = false;
             }
-            return false;
+        }
+        return false;
     }
+
 public:
     string smallestNumber(string pattern) {
         int n = pattern.size();
-        string curr = "", ans = "";
+        string ans = "";
         bitset<10> isUsed;
-        getNum(0, n, pattern, curr, ans, isUsed);
+        getNum(0, n, pattern, "", ans, isUsed);
         return ans;
     }
 };
