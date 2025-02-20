924e39379dc6b45e357f1bc19a3a5bf6e7c504f8
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-20 07:02:30 +0530

    Time: 0 ms (100%), Space: 12.6 MB (84%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,28 +1,14 @@
 class Solution {
-    bool find(int n, unordered_set<string>& hash, string& curr, string& ans) {
-        if (curr.size() == n) {
-            if (hash.find(curr) == hash.end()) {
-                ans = curr;
-                return true;
-            }
-            return false;
-        }
-
-        curr += "0";
-        if (find(n, hash, curr, ans)) return true;
-        curr.pop_back();
-
-        curr += "1";
-        if (find(n, hash, curr, ans)) return true;
-        curr.pop_back();
-
-        return false;
-    }
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<string> hash(nums.begin(), nums.end());
-        string ans = "", curr = "";
-        find(nums.size(), hash, curr, ans);
+        // Cantor's diagonalization
+        int n = nums.size();
+        string ans = "";
+
+        for (int i = 0; i < n; i++) {
+            ans += nums[i][i] == '0' ? '1' : '0';
+        }
+
         return ans;
     }
 };
