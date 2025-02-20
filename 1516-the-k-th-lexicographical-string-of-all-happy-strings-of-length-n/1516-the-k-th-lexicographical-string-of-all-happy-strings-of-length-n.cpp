1a1db4306e886bf2eb3afba31fae7660975c3d0f
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-20 22:54:47 +0530

    Time: 23 ms (34.82%), Space: 25.5 MB (28.35%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,38 +1,20 @@
 class Solution {
-public:
-    string getHappyString(int n, int k) {
-        string currentString = "";
-        vector<string> happyStrings;
-        // Generate all happy strings of length n
-        generateHappyStrings(n, currentString, happyStrings);
-
-        // Check if there are at least k happy strings
-        if (happyStrings.size() < k) return "";
-
-        // Sort the happy strings in lexicographical order
-        sort(happyStrings.begin(), happyStrings.end());
-
-        return happyStrings[k - 1];
-    }
-
-private:
-    void generateHappyStrings(int n, string currentString,
-                              vector<string> &happyStrings) {
-        // If the current string has reached the desired length, add it to the
-        // list
-        if (currentString.size() == n) {
-            happyStrings.push_back(currentString);
+    void generate(int idx, int n, string happyString, vector<string>& happyStrings) {
+        if (idx == n) {
+            happyStrings.push_back(happyString);
             return;
         }
 
-        // Try adding each character ('a', 'b', 'c') to the current string
-        for (char currentChar = 'a'; currentChar <= 'c'; currentChar++) {
-            // Skip if the current character is the same as the last character
-            if (currentString.size() > 0 && currentString.back() == currentChar)
-                continue;
-
-            // Recursively generate the next character
-            generateHappyStrings(n, currentString + currentChar, happyStrings);
+        for (int i = 0; i < 3; i++) {
+            char ch = ('a' + i);
+            if (idx == 0 || happyString[idx - 1] != ch) 
+                generate(idx + 1, n, happyString + ch, happyStrings);
         }
+    }
+public:
+    string getHappyString(int n, int k) {
+        vector<string> happyStrings;
+        generate(0, n, "", happyStrings);
+        return k <= happyStrings.size() ? happyStrings[k - 1] : "";
     }
 };
