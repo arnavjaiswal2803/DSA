85579829b48397fbeff65ba1169d9470c6faaf60
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-02-21 11:29:08 +0530

    Time: 8 ms (41.18%), Space: 8.9 MB (49.25%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,21 +1,23 @@
 class Solution {
-    void count(string tiles, string curr, unordered_set<string>& hash) {
-        hash.insert(curr);
+    void count(string tiles, string curr, int& ans) {
+        ans++;
         if (tiles.size() == 0) return;
 
         for (int i = 0; i < tiles.size(); i++) {
+            if (i > 0 && tiles[i] == tiles[i - 1]) continue;
             string newTiles = tiles.substr(0, i) + tiles.substr(i + 1);
-            count(newTiles, curr + tiles[i], hash);
+            count(newTiles, curr + tiles[i], ans);
         }
     }
 public:
     int numTilePossibilities(string tiles) {
-        int n = tiles.size();
-        unordered_set<string> hash;
+        int n = tiles.size(), ans = 0;
+        sort(tiles.begin(), tiles.end());
         for (int i = 0; i < n; i++) {
+            if (i > 0 && tiles[i] == tiles[i - 1]) continue;
             string newTiles = tiles.substr(0, i) + tiles.substr(i + 1);
-            count(newTiles, to_string(tiles[i]), hash);
+            count(newTiles, to_string(tiles[i]), ans);
         }
-        return hash.size();
+        return ans;
     }
 };
