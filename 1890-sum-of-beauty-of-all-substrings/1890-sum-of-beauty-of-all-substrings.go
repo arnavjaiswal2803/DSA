d9add20247fb537dec8ebcc24eb481d187c1dcfe
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-03-04 23:49:07 +0530

    Time: 65 ms (61.9%), Space: 4 MB (80.95%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.go
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.go
@@ -1,26 +1,20 @@
 func beautySum(s string) int {
-    n, beautySum := len(s), 0
-    for i := 0; i < n; i++ {
-        for j := i; j < n; j++ {
-            beautySum += getBeauty(&s, i, j)
-        }
-    }
-    return beautySum
-}
+	n, beautySum := len(s), 0
+	for i := 0; i < n; i++ {
+		freq := make([]int, 26)
+		for j := i; j < n; j++ {
+			freq[s[j]-'a']++
+            
+			maxFreq, leastFreq := 0, n
+			for i := 0; i < 26; i++ {
+				maxFreq = max(maxFreq, freq[i])
+				if freq[i] > 0 && (leastFreq == n || freq[i] < leastFreq) {
+					leastFreq = freq[i]
+				}
+			}
 
-func getBeauty(s *string, start, end int) int {
-    n, freq := len(*s), make([]int, 26)
-    for i := start; i <= end; i++ {
-        freq[(*s)[i] - 'a']++
-    }
-
-    maxFreq, leastFreq := 0, n
-    for i := 0; i < 26; i++ {
-        maxFreq = max(maxFreq, freq[i])
-        if (freq[i] > 0 && (leastFreq == n || freq[i] < leastFreq)) {
-            leastFreq = freq[i]
-        }
-    }
-
-    return maxFreq - leastFreq
+			beautySum += maxFreq - leastFreq
+		}
+	}
+	return beautySum
 }
