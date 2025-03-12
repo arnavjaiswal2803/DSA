30104828b7de51f63e01f7de39399da45ecc6291
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-03-12 23:33:48 +0530

    Time: 0 ms (100%), Space: 8.1 MB (7.77%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.go
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.go
@@ -1,11 +1,34 @@
 func maximumCount(nums []int) int {
-    posCnt, negCnt := 0, 0
-    for _, val := range nums {
-        if val < 0 {
-            negCnt++
-        } else if val > 0 {
-            posCnt++
+    n := len(nums)
+    posCnt := n - getFirstOccurenceOfPositive(nums)
+    negCnt := 1 + getLastOccurenceOfNegative(nums)
+    return max(posCnt, negCnt)
+}
+
+func getLastOccurenceOfNegative(nums []int) int {
+    low, high := 0, len(nums) - 1
+    for low <= high {
+        mid := (low + ((high - low) >> 1))
+
+        if nums[mid] < 0 {
+            low = mid + 1
+        } else {
+            high = mid - 1
         }
     }
-    return max(posCnt, negCnt)
+    return high
+}
+
+func getFirstOccurenceOfPositive(nums []int) int {
+    low, high := 0, len(nums) - 1
+    for low <= high {
+        mid := (low + ((high - low) >> 1))
+
+        if nums[mid] > 0 {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return low
 }
