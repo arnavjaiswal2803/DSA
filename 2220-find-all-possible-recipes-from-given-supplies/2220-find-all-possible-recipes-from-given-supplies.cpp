fa0e4aeba0da6e363c3a170e656eec5c34af4c20
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-03-21 13:23:13 +0530

    Time: 141 ms (48.27%), Space: 165 MB (33.93%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -3,30 +3,34 @@ public:
     vector<string> findAllRecipes(vector<string>& recipes,
                                   vector<vector<string>>& ingredients,
                                   vector<string>& supplies) {
-        int n = recipes.size(), ansSize = -1;
-        unordered_set<string> suppliesSet(supplies.begin(), supplies.end());
-        vector<string> ans;
+        int n = recipes.size();
+        unordered_map<string, int> inDeg;
+        unordered_map<string, vector<string>> adjList;
+    
+        for (int i = 0; i < n; i++) {
+            for (string &ingredient : ingredients[i]) {
+                adjList[ingredient].push_back(recipes[i]);
+                inDeg[recipes[i]]++;
+            }
+        }
 
-        while (ansSize != ans.size()) {
-            ansSize = ans.size();
-            
-            for (int i = 0; i < n; i++) {
-                if (suppliesSet.find(recipes[i]) != suppliesSet.end()) continue;
-                
-                bool canMake = true;
-                for (string& ingredient : ingredients[i]) {
-                    if (suppliesSet.find(ingredient) == suppliesSet.end()) {
-                        canMake = false;
-                        break;
-                    }
-                }
-                if (canMake) {
-                    ans.push_back(recipes[i]);
-                    suppliesSet.insert(recipes[i]);
+        vector<string> possibleRecipes;
+        queue<string> q;
+        for (string &supply : supplies) q.push(supply);
+
+        while (!q.empty()) {
+            string ingredient = q.front();
+            q.pop();
+
+            for (string &recipe : adjList[ingredient]) {
+                inDeg[recipe]--;
+                if (inDeg[recipe] == 0) {
+                    possibleRecipes.push_back(recipe);
+                    q.push(recipe);
                 }
             }
         }
 
-        return ans;
+        return possibleRecipes;
     }
 };
