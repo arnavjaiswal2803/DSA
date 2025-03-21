aad40d0538b3f25eab7d005c8b6b6b764dd5e3c6
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-03-21 12:22:22 +0530

    Time: 607 ms (12.37%), Space: 98.7 MB (99.9%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -3,11 +3,13 @@ public:
     vector<string> findAllRecipes(vector<string>& recipes,
                                   vector<vector<string>>& ingredients,
                                   vector<string>& supplies) {
-        int n = recipes.size();
+        int n = recipes.size(), ansSize = -1;
         unordered_set<string> suppliesSet(supplies.begin(), supplies.end());
         vector<string> ans;
 
-        for (int cnt = 0; cnt < n; cnt++) {
+        while (ansSize != ans.size()) {
+            ansSize = ans.size();
+            
             for (int i = 0; i < n; i++) {
                 if (suppliesSet.find(recipes[i]) != suppliesSet.end()) continue;
                 
