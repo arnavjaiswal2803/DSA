23aedebd6b0d42f2ae84a166745a788020405e34
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-03-21 14:43:05 +0530

    Time: 425 ms (21.2%), Space: 208.8 MB (59.13%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,74 +1,68 @@
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> depth;
-
     vector<int> minimumCost(int n, vector<vector<int>>& edges,
-                            vector<vector<int>>& queries) {
-        // Initialize the parent array with -1 as initially each node belongs to
-        // its own component
-        parent.resize(n, -1);
-        depth.resize(n, 0);
+                            vector<vector<int>>& query) {
+        DisjointSet ds(n);
 
-        // All values are initially set to the number with only 1s in its binary
-        // representation
-        vector<unsigned int> componentCost(n, -1);
-
-        // Construct the connected components of the graph
-        for (auto& edge : edges) {
-            Union(edge[0], edge[1]);
+        for (vector<int> &edge : edges) {
+            int u = edge[0], v = edge[1];
+            ds.unionByRank(u, v);
         }
 
-        // Calculate the cost of each component by performing bitwise AND of all
-        // edge weights in it
-        for (auto& edge : edges) {
-            int root = find(edge[0]);
-            componentCost[root] &= edge[2];
-        }
+        unordered_map<int, int> componentMinCost;
+        for (vector<int> &edge : edges) {
+            int u = edge[0], wt = edge[2];
+            int ultiPar = ds.findUltimateParent(u);
 
-        vector<int> answer;
-        for (auto& query : queries) {
-            int start = query[0];
-            int end = query[1];
-
-            // If the two nodes are in different connected components, return -1
-            if (find(start) != find(end)) {
-                answer.push_back(-1);
+            if (componentMinCost.find(ultiPar) == componentMinCost.end()) {
+                componentMinCost[ultiPar] = wt;
             } else {
-                // Find the root of the edge's component
-                int root = find(start);
-                // Return the precomputed cost of the component
-                answer.push_back(componentCost[root]);
+                componentMinCost[ultiPar] &= wt;
             }
         }
-        return answer;
+
+        int k = query.size();
+        vector<int> ans(k);
+        for (int i = 0; i < k; i++) {
+            int s = query[i][0], t = query[i][1];
+            int ultiParS = ds.findUltimateParent(s), 
+                ultiParT = ds.findUltimateParent(t);
+
+            ans[i] = ultiParS == ultiParT ? componentMinCost[ultiParS] : -1;
+        }
+
+        return ans;
     }
 
 private:
-    // Find function to return the root (representative) of a node's component
-    int find(int node) {
-        // If the node is its own parent, it is the root of the component
-        if (parent[node] == -1) return node;
-        // Otherwise, recursively find the root and apply path compression
-        return parent[node] = find(parent[node]);
-    }
+    class DisjointSet {
+        vector<int> parent;
+        vector<long long> rank;
 
-    // Union function to merge the components of two nodes
-    void Union(int node1, int node2) {
-        int root1 = find(node1);
-        int root2 = find(node2);
+    public:
+        DisjointSet(int n) {
+            rank.resize(n + 1, 1);
+            parent.resize(n + 1);
+            for (int i = 0; i <= n; i++) parent[i] = i;
+        }
 
-        // If the two nodes are already in the same component, do nothing
-        if (root1 == root2) return;
+        int findUltimateParent(int u) {
+            if (parent[u] == u) return u;
+            return parent[u] = findUltimateParent(parent[u]); 
+        }
 
-        // Union by depth: ensure the root of the deeper tree becomes the parent
-        if (depth[root1] < depth[root2]) swap(root1, root2);
+        void unionByRank(int u, int v) {
+            int ultiParU = findUltimateParent(u);
+            int ultiParV = findUltimateParent(v);
 
-        // Merge the two components by making root1 the parent of root2
-        parent[root2] = root1;
-
-        // If both components had the same depth, increase the depth of the new
-        // root
-        if (depth[root1] == depth[root2]) depth[root1]++;
-    }
+            if (rank[ultiParU] < rank[ultiParV]) {
+                parent[ultiParU] = ultiParV;
+            } else if (rank[ultiParU] > rank[ultiParV]) {
+                parent[ultiParV] = ultiParU;
+            } else {
+                parent[ultiParV] = ultiParU;
+                rank[ultiParV]++;
+            }
+        }
+    };
 };
