1584268dfdc086361cc6caeae678d9139e97fc9b
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-03-25 13:01:35 +0530

    Time: 124 ms (72.65%), Space: 185.5 MB (95.4%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
     bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
-        return canMakeHorizontalCuts(rectangles) ||
-               canMakeVerticalCuts(rectangles);
+        return canMakeTwoCuts(rectangles, 0) ||
+               canMakeTwoCuts(rectangles, 1);
     }
 
 private:
-    bool canMakeHorizontalCuts(vector<vector<int>>& rectangles) {
+    bool canMakeTwoCuts(vector<vector<int>>& rectangles, int dim) {
         int m = rectangles.size();
 
-        // sorting according to y-start
         sort(rectangles.begin(), rectangles.end(),
-             [](vector<int>& a, vector<int>& b) { return a[1] < b[1]; });
+             [dim](vector<int>& a, vector<int>& b) { return a[dim] < b[dim]; });
 
-        int cuts = 0, maxEnd = rectangles[0][3];
+        int cuts = 0, maxEnd = rectangles[0][dim + 2];
         for (int i = 1; i < m; i++) {
-            if (maxEnd <= rectangles[i][1]) {
+            if (maxEnd <= rectangles[i][dim]) {
                 cuts++;
                 if (cuts >= 2) return true;
             }
-            maxEnd = max(maxEnd, rectangles[i][3]);
+            maxEnd = max(maxEnd, rectangles[i][dim + 2]);
         }
-        return false;
-    }
-
-    bool canMakeVerticalCuts(vector<vector<int>>& rectangles) {
-        int m = rectangles.size();
-
-        // sorting according to x-start
-        sort(rectangles.begin(), rectangles.end(),
-             [](vector<int>& a, vector<int>& b) { return a[0] < b[0]; });
-
-        int cuts = 0, maxEnd = rectangles[0][2];
-        for (int i = 1; i < m; i++) {
-            if (maxEnd <= rectangles[i][0]) {
-                cuts++;
-                if (cuts >= 2) return true;
-            }
-            maxEnd = max(maxEnd, rectangles[i][2]);
-        }
-
         return false;
     }
 };
