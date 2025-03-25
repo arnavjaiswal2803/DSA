74e2c43353b5fccbb1b73deb27681c4081e30c41
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-03-25 12:57:48 +0530

    Time: 103 ms (79.65%), Space: 185.5 MB (95.4%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -1,48 +1,45 @@
 class Solution {
-    const bool X_AXIS = true;
-    const bool Y_AXIS = false;
-
-    bool doesOverlap(vector<int>& a, vector<int>& b) {
-        int s1 = a[0], e1 = a[1];
-        int s2 = b[0], e2 = b[1];
-        return s2 < e1 && s1 < e2;
-    }
-
-    vector<vector<int>> mergeIntervals(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        vector<vector<int>> merged;
-
-        sort(intervals.begin(), intervals.end());
-
-        merged.push_back(intervals[0]);
-        for (int i = 1; i < n; i++) {
-            if (!doesOverlap(merged.back(), intervals[i])) 
-                merged.push_back(intervals[i]);
-            else {
-                vector<int> newInterval = {
-                    min(merged.back()[0], intervals[i][0]),
-                    max(merged.back()[1], intervals[i][1])
-                };
-                merged.pop_back();
-                merged.push_back(newInterval);
-            } 
-        }
-
-        return merged;
-    }
-
-    bool canCut(vector<vector<int>>& rectangles, bool axis) {
-        vector<vector<int>> coordinates;
-        for (auto &rectangle : rectangles) {
-            int coordinate1 = axis == X_AXIS ? 0 : 1;
-            int coordinate2 = axis == X_AXIS ? 2 : 3;
-            coordinates.push_back({rectangle[coordinate1], rectangle[coordinate2]});
-        }
-        coordinates = mergeIntervals(coordinates);
-        return coordinates.size() >= 3;
-    }
 public:
     bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
-        return canCut(rectangles, X_AXIS) || canCut(rectangles, Y_AXIS);
+        return canMakeHorizontalCuts(rectangles) ||
+               canMakeVerticalCuts(rectangles);
+    }
+
+private:
+    bool canMakeHorizontalCuts(vector<vector<int>>& rectangles) {
+        int m = rectangles.size();
+
+        // sorting according to y-start
+        sort(rectangles.begin(), rectangles.end(),
+             [](vector<int>& a, vector<int>& b) { return a[1] < b[1]; });
+
+        int cuts = 0, maxEnd = rectangles[0][3];
+        for (int i = 1; i < m; i++) {
+            if (maxEnd <= rectangles[i][1]) {
+                cuts++;
+                if (cuts >= 2) return true;
+            }
+            maxEnd = max(maxEnd, rectangles[i][3]);
+        }
+        return false;
+    }
+
+    bool canMakeVerticalCuts(vector<vector<int>>& rectangles) {
+        int m = rectangles.size();
+
+        // sorting according to x-start
+        sort(rectangles.begin(), rectangles.end(),
+             [](vector<int>& a, vector<int>& b) { return a[0] < b[0]; });
+
+        int cuts = 0, maxEnd = rectangles[0][2];
+        for (int i = 1; i < m; i++) {
+            if (maxEnd <= rectangles[i][0]) {
+                cuts++;
+                if (cuts >= 2) return true;
+            }
+            maxEnd = max(maxEnd, rectangles[i][2]);
+        }
+
+        return false;
     }
 };
