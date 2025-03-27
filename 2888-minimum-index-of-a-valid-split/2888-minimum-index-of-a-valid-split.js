f0f1ef6438e565b0ccf06b95ab6009777800a041
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-03-27 23:39:15 +0530

    Time: 8 ms (83.33%), Space: 68.3 MB (83.33%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.js
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.js
@@ -6,20 +6,25 @@ var minimumIndex = function(nums) {
     const n = nums.length;
     const dominantElem = findDominantElement(nums, n);
 
-    const prefixFreq = new Array(n);
-    prefixFreq[0] = nums[0] === dominantElem ? 1 : 0;
-    for (let i = 1; i < n; i++) {
-        prefixFreq[i] = prefixFreq[i - 1] + (nums[i] === dominantElem ? 1 : 0);
+    let dominantElemFreq = 0;
+    for (let i = 0; i < n; i++) {
+        if (nums[i] === dominantElem) {
+            dominantElemFreq++;
+        }
     }
 
+    let freqTillI = 0;
     for (let i = 0; i < n - 1; i++) {
         // first subarray
-        const size1 = i + 1, dominantElemFreq1 = prefixFreq[i];
-        if (dominantElemFreq1 <= parseInt(size1 / 2)) continue;
+        const size1 = i + 1;
+        if (nums[i] === dominantElem) {
+            freqTillI++;
+        }
+        if (freqTillI <= parseInt(size1 / 2)) continue;
 
         // second subarray
         const size2 = n - i - 1;
-        const dominantElemFreq2 = prefixFreq[n - 1] - prefixFreq[i];
+        const dominantElemFreq2 = dominantElemFreq - freqTillI;
         if (dominantElemFreq2 <= parseInt(size2 / 2)) continue;
 
         return i;
