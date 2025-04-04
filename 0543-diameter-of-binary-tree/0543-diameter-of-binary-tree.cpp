736681b3d482d78793193b8b1e38ac1c0b363022
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-04-04 15:43:53 +0530

    Time: 3 ms (25.95%), Space: 26 MB (5.01%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -6,24 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-    int findDiameter(TreeNode* root, int &diameter) {
-        if (root == NULL) return 0;
-
-        int left = findDiameter(root->left, diameter);
-        int right = findDiameter(root->right, diameter);
-
-        diameter = max(diameter, left + right);
-
-        return 1 + max(left, right);
-    }
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0;
-        findDiameter(root, diameter);
-        return diameter;
+        int maxDia = 0;
+        calculateDiameter(root, &maxDia);
+        return maxDia;
+    }
+
+private:
+    int calculateDiameter(TreeNode* root, int* maxDia) {
+        if (root == NULL) return 0;
+
+        int leftDia = calculateDiameter(root->left, maxDia);
+        int rightDia = calculateDiameter(root->right, maxDia);
+
+        *maxDia = max({*maxDia, leftDia, rightDia, leftDia + rightDia});
+
+        return 1 + max(leftDia, rightDia);
     }
 };
