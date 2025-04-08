95d1b18ad56820835ba5064e650553307e62cc02
Arnav Jaiswal <79758749+arnavjaiswal2803@users.noreply.github.com>
2025-04-08 23:17:41 +0530

    Time: 5 ms (43.48%), Space: 11.3 MB (39.06%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.go
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.go
@@ -1,22 +1,10 @@
 func maximumTripletValue(nums []int) int64 {
-	n := len(nums)
-	prefixMax, suffixMax := make([]int, n), make([]int, n)
-
-	prefixMax[0] = nums[0]
-	for i := 1; i < n; i++ {
-		prefixMax[i] = max(prefixMax[i-1], nums[i])
-	}
-
-	suffixMax[n-1] = nums[n-1]
-	for i := n - 2; i >= 0; i-- {
-		suffixMax[i] = max(suffixMax[i+1], nums[i])
-	}
-
-	maxVal := int64(0)
-	for i := 1; i < n-1; i++ {
-		maxVal = max(maxVal,
-			(int64(prefixMax[i-1])-int64(nums[i]))*int64(suffixMax[i+1]))
-	}
-
-	return maxVal
+    n := len(nums)
+    maxI, maxDiff, maxVal := int64(0), int64(0), int64(0)
+    for k := 0; k < n; k++ {
+        maxVal = max(maxVal, maxDiff * int64(nums[k]))
+        maxDiff = max(maxDiff, maxI - int64(nums[k]))
+        maxI = max(maxI, int64(nums[k]))
+    }
+    return maxVal
 }
